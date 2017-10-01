b7c0d442e21f68495a098227da7b8ed5057d0dda
Tomás Ferreira <ttomaz.200@gmail.com>
2017-10-01 19:53:02 +0100

    tables with expandable rows (waterfall)

--- Client/Scripts/components.vue
+++ Client/Scripts/components.vue
@@ -14,9 +14,9 @@ Vue.component('custom-table-simple', {
             <div class="col-sm-7">
                 <input class="form-control" v-model="filt" type="text" placeholder="Filtrar"/>
             </div>
-            <div class="col-sm-1">
-                <button v-if="add" type="button" class="btn btn-default btn-circle" @click="addClick">
-                    +
+            <div class="col-sm-1"  v-if="add">
+                <button type="button" class="btn btn-default btn-circle" @click="addClick">
+                    <span class="glyphicon glyphicon-plus"/>
                 </button>
             </div>
             
@@ -42,7 +42,14 @@ Vue.component('custom-table-simple', {
             </div>
         </div>
     `,
-    props: ['classTable','completeRows','header','ready','cwidth','add'],
+    props: [
+        'classTable',
+        'completeRows',
+        'header',
+        'ready',
+        'cwidth',
+        'add'
+    ],
     data: function(){ 
         return{
             "rows": [],
@@ -71,7 +78,7 @@ Vue.component('custom-table-simple', {
         },
     },
     methods: {
-        filter: function () {
+        filter: function () { //filter rows according to what is written in the input box
             regex= new RegExp(this.filt,"gi");
                 
             this.rows=this.completeRows.filter(function (item) {
@@ -87,7 +94,7 @@ Vue.component('custom-table-simple', {
                 this.rows=[["Sem resultados correspondentes..."]];
             }
         },
-        sort: function(index) {
+        sort: function(index) { //sort rows by header[index]
             if(this.order==index){
                 this.rows.reverse();
                 this.order=-index;
@@ -101,17 +108,13 @@ Vue.component('custom-table-simple', {
                 this.order=index;
             }
         },
-        populate: function(){
-            this.rows=this.completeRows;
-            this.prepPage();
-        },
-        rowClick: function(index) {
+        rowClick: function(index) { //emit event when a row is clicked 
             this.$emit('row-clicked', this.rowsShow[index]);
         },
-        addClick: function(index) {
+        addClick: function(index) { //emit event when the '+' button is clicked
             this.$emit('add-clicked');
         },
-        loadPages: function(){
+        loadPages: function(){ //process page numbers
             var page=this.activePage;
             var ret = [];
             
@@ -144,12 +147,12 @@ Vue.component('custom-table-simple', {
                 this.prepPage();
             }
         },
-        loadPage: function(page){
+        loadPage: function(page){ //change active page
             if(page!=this.activePage && page>0 && page<=this.nPages){
                 this.activePage=page;
             }
         },
-        prepPage: function(){  
+        prepPage: function(){ //process rows to be shown 
             var beggining = (this.activePage-1)*this.rowsPerPage;
             var end = beggining+parseInt(this.rowsPerPage);
 
@@ -161,7 +164,7 @@ Vue.component('custom-table-simple', {
         prevPage: function(){
             this.loadPage(this.activePage-1);
         },
-        firstLoad: function(){
+        firstLoad: function(){ //loads info upon first render
             var ret = [];
             
             var n = Math.ceil(this.completeRows.length/this.rowsPerPage);
@@ -182,10 +185,104 @@ Vue.component('custom-table-simple', {
 })
 
 
+Vue.component('row-waterfall', {
+    template: `
+        <tr>
+            <td colspan=4 :class="[root ? 'cascata-row-root' : 'cascata-row']">
+                <table :class="tableClass">
+                    <tbody name="table">
+                        <tr>
+                            <td v-if="row.sublevel" 
+                                class="cascata-drop"
+                            >
+                                <label
+                                    :for="'toggle'+id"
+                                    :class="[drop ? 'glyphicon glyphicon-chevron-down' : 'glyphicon glyphicon-chevron-right']"
+                                />
+                                <input
+                                    :id="'toggle'+id" 
+                                    type="checkbox" 
+                                    v-model="drop" 
+                                    @click="dropClicked"
+                                    class="drop-row"
+                                /> 
+                            </td>
+                            <td v-else 
+                                class="cascata-drop" 
+                            >
+                            </td>
+                            <td 
+                                v-for="(item,index) in row.content"
+                                :style="{width: cwidth[index]}"
+                                @click="rowClicked"
+                            > 
+                                {{ item }} 
+                            </td>
+                        </tr>
+                        
+                        <row-waterfall v-if="drop && subReady[id]" 
+                            v-for="(line,index) in row.sublevel"
+
+                            :id="genId(index)" 
+                            :row="line" 
+                            :key="index"  
+                            :cwidth="cwidth"
+                            :sub-ready="subReady"
+
+                            @eventWaterfall="eventPass($event)"
+
+                            :table-class="tableClass+' cascata'"
+                        />
+                        
+                        <tr v-if="drop && !subReady[id]">
+                            <td colspan=4> Loading... </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr> 
+    `,
+    props: ['row', 'tableClass', 'cwidth', 'id', 'subReady', 'root'],
+    data: function () {
+        return {
+            drop: false,
+        }
+    },
+    methods: {
+        dropClicked: function () { //emit event when a row is expanded
+            var eventContent = {
+                type: "drop",
+                params: {
+                    id: this.id,
+                    rowData: this.row.content
+                }
+            };
+            this.$emit('eventWaterfall', eventContent);
+        },
+        rowClicked: function () { //emit event when a row is clicked
+            var eventContent = {
+                type: "row",
+                params: {
+                    id: this.id,
+                    rowData: this.row.content
+                }
+            };
+            this.$emit('eventWaterfall', eventContent);
+        },
+        genId: function (index) { //generate an ID for the child component
+            return this.id + "." + index;
+        },
+        eventPass: function (event) { //pass up an event from child component
+            this.$emit('eventWaterfall', event);
+        }
+    },
+})
+
+
 Vue.component('custom-table-waterfall', {
     template: `
         <div>
-            <div class="col-sm-4">
+            <div class="col-sm-6" v-if="pagesOn">
                 Mostrar 
                 <select v-model="rowsPerPage">
                     <option>5</option>
@@ -195,174 +292,188 @@ Vue.component('custom-table-waterfall', {
                 </select>
                 entradas
             </div>
-            <div class="col-sm-7">
-                <input class="form-control" v-model="filt" type="text" placeholder="Filtrar"/>
-            </div>
-            <div class="col-sm-1">
-                <button v-if="add" type="button" class="btn btn-default btn-circle" @click="addClick">
-                    +
-                </button>
+            <div class="col-sm-6" v-if="filterOn">
+                <input 
+                    class="form-control" 
+                    v-model="filt" type="text" 
+                    placeholder="Filtrar"
+                />
             </div>
             
-            <table :class="classTable">
+            <table :class="tableClass">
                 <thead>
                     <tr>
-                        <th v-for="(item,index) in header" @click="sort(index)" class="sorter" :style="{width: cwidth[index]}">
+                        <th style="width: 4%"></th>
+                        <th 
+                            v-for="(item,index) in header" 
+                            @click="sort(index)" 
+                            class="sorter" 
+                            :style="{width: cwidth[index]}"
+                        >
                             {{ item }} <span class="caret"></span>
                         </th>
                     </tr>
                 </thead>
                 <tbody name="table">
-                    <tr v-for="(row,index) in rowsShow" :key="row[0]" @click="rowClick(index)">
-                        <td v-for="item in row" style="max-width: 0; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ item }}</td>
-                    </tr>
+                    <row-waterfall 
+                        v-for="(row,index) in rowsShow"
+
+                        :row="row" 
+                        :id="completeRows.indexOf(row)+''" 
+                        :key="row.index" 
+                        :sub-ready="subReady" 
+                        :cwidth="cwidth" 
+                        
+                        :table-class="tableClass+' cascata'" 
+                        root="true" 
+                        
+                        @eventWaterfall="eventPass($event)"
+                    />
                 </tbody>
             </table>
             
-            <div class="pages">
+            <div class="pages" v-if="pagesOn">
                 <span class="glyphicon glyphicon-chevron-left page-number" @click="prevPage"></span>
-                <span class="page-number btn btn-xs btn-default" v-for="page in pages" :class="{active: page==activePage}" @click="loadPage(page)" :disabled="isNaN(page)">\ {{ page }}\ </span>
+                <span class="page-number btn btn-xs btn-default" v-for="page in pages" :class="{active: page==activePage}" @click="loadPage(page)">\ {{ page }}\ </span>
                 <span class="glyphicon glyphicon-chevron-right page-number" @click="nextPage"></span>
             </div>
         </div>
-        `,
-    props: ['classTable','completeRows','header','ready','cwidth','add'],
-    data: function(){ 
-        return{
+    `,
+    props: [
+        'cwidth',
+        'table-class',
+        'url',
+        'completeRows',
+        'header',
+        'ready',
+        'subReady',
+        'pagesOn',
+        'filterOn'
+    ],
+    data: function () {
+        return {
             "rows": [],
-            "rowsShow" : [[]],
-            "filt" : '',
-            "order" : 0,
-            "activePage" : 1,
-            "pages" : [0],
+            "rowsShow": [[]],
+            "filt": '',
+            "order": 0,
+            "activePage": 1,
+            "pages": [0],
             "rowsPerPage": 10,
-            "nPages": 1,
         };
     },
     watch: {
-        filt: function(){
+        filt: function () {
             this.filter();
         },
-        rows: function(){
+        rows: function () {
             this.loadPages();
         },
-        rowsPerPage: function(){
+        rowsPerPage: function () {
             this.loadPages();
         },
-        activePage: function(){
-            this.loadPages();
+        activePage: function () {
             this.prepPage();
         },
     },
     methods: {
-        filter: function () {
-            regex= new RegExp(this.filt,"gi");
-                
-            this.rows=this.completeRows.filter(function (item) {
-                    
-                for (var i=0; i<item.length; i++) {
-                    if(regex.test(item[i])){
+        filter: function () { //filter rows according to what is written in the input box
+            regex = new RegExp(this.filt, "gi");
+
+            this.rows = this.completeRows.filter(function (item) {
+
+                for (var i = 0; i < item.content.length; i++) {
+                    if (regex.test(item.content[i])) {
                         return true;
                     }
                 }
                 return false;
             })
-            if(this.rows.length==0){
-                this.rows=[["Sem resultados correspondentes..."]];
+            console.log("ola");
+            if (this.rows.length == 0) {
+                this.rows = [{ content: ["Sem resultados correspondentes..."] }];
             }
         },
-        sort: function(index) {
-            if(this.order==index){
+        sort: function (index) { //sort rows by header[index]
+            if (this.order == index) {
                 this.rows.reverse();
-                this.order=-index;
+                this.order = -index;
             } else {
-                this.rows.sort(function(a,b) {
-                    if (typeof a[index] === 'string' || a[index] instanceof String)
-                        return a[index].localeCompare(b[index]);
+                this.rows.sort(function (a, b) {
+                    if (typeof a.content[index] === 'string' || a.content[index] instanceof String)
+                        return a.content[index].localeCompare(b.content[index]);
                     else
-                        return a[index]-b[index];
+                        return a.content[index] - b.content[index];
                 })
-                this.order=index;
+                this.order = index;
             }
         },
-        populate: function(){
-            this.rows=this.completeRows;
-            this.prepPage();
-        },
-        rowClick: function(index) {
-            this.$emit('row-clicked', this.rowsShow[index]);
-        },
-        addClick: function(index) {
-            this.$emit('add-clicked');
-        },
-        loadPages: function(){
-            var page=this.activePage;
+        loadPages: function () { //process page numbers 
             var ret = [];
-            
-            var n = Math.ceil(this.rows.length/this.rowsPerPage);         
 
-            this.nPages=n;
+            var n = Math.ceil(this.rows.length / this.rowsPerPage);
 
-            if(n>7){
-                if(page<5){
-                    ret=[1,2,3,4,5,"...",n];
-                }
-                else if(page>n-4){
-                    ret=[1,"...",n-4,n-3,n-2,n-1,n];
-                }
-                else{
-                    ret=[1,"...",page-1,page,page+1,"...",n];
-                }
-            }
-            else {
-                for(var i=1; i<=n; i++) {
-                    ret.push(i);
-                }
+            for (var i = 1; i <= n; i++) {
+                ret.push(i);
             }
 
-            this.pages=ret;
+            this.pages = ret;
 
-            if(page>n){
+            if (this.activePage > n) {
                 this.loadPage(n);
             } else {
                 this.prepPage();
             }
         },
-        loadPage: function(page){
-            if(page!=this.activePage && page>0 && page<=this.nPages){
-                this.activePage=page;
+        loadPage: function (page) { //change active page
+            if (page != this.activePage && page > 0 && page <= this.pages.length) {
+                this.activePage = page;
             }
         },
-        prepPage: function(){  
-            var beggining = (this.activePage-1)*this.rowsPerPage;
-            var end = beggining+parseInt(this.rowsPerPage);
+        prepPage: function () { //process rows to be shown 
+            var beggining = (this.activePage - 1) * this.rowsPerPage;
+            var end = beggining + parseInt(this.rowsPerPage);
 
-            this.rowsShow=this.rows.slice(beggining,end);        
+            this.rowsShow = this.rows.slice(beggining, end);
         },
-        nextPage: function(){
-            this.loadPage(this.activePage+1);
+        nextPage: function () {
+            this.loadPage(this.activePage + 1);
         },
-        prevPage: function(){
-            this.loadPage(this.activePage-1);
+        prevPage: function () {
+            this.loadPage(this.activePage - 1);
         },
-        firstLoad: function(){
+        firstLoad: function () { //loads info upon first render
             var ret = [];
-            
-            var n = Math.ceil(this.completeRows.length/this.rowsPerPage);
 
-            for(var i=1; i<=n; i++) {
+            var n = Math.ceil(this.completeRows.length / this.rowsPerPage);
+
+            for (var i = 1; i <= n; i++) {
                 ret.push(i);
             }
 
-            this.pages=ret;
+            this.pages = ret;
 
-            this.rowsShow=this.completeRows.slice(0,this.rowsPerPage);
+            this.rowsShow = this.completeRows.slice(0, this.rowsPerPage);
+        },
+        eventPass: function (event) { //process event comming from child component
+            if (event.type == "row") {
+                this.rowClick(event.params);
+            }
+            else if (event.type == "drop") {
+                this.dropClick(event.params);
+            }
+        },
+        rowClick: function (params) { //emit event when a row is clicked
+            this.$emit('row-clicked', params);
+        },
+        dropClick: function (params) { //emit event when a row is expanded
+            this.$emit('drop-clicked', params);
         },
     },
-    beforeMount: function(){
+    beforeMount: function () {
         this.firstLoad();
-        this.rows=this.completeRows;
+        this.rows = this.completeRows;
     }
 })
+
 
 
