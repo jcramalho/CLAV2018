ccf76bc87496ea2726fd4c2ce2924a3ea140ed14
Tomás Ferreira <ttomaz.200@gmail.com>
2017-11-07 13:17:23 +0000

    tweaks

--- Client/Scripts/TabsSel/adicao.vue
+++ Client/Scripts/TabsSel/adicao.vue
@@ -110,9 +110,6 @@ var selecao = new Vue({
                 this.loadSub(tail, newLocation, params);
             }
         },
-        rowClicked: function (params) {
-            window.location.href = '/classe?id=c' + params.rowData[0];
-        },
         parse: function () {
             // parsing the JSON
             for (var i = 0; i < this.content.length; i++) {
