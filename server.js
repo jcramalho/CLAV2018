3952362ca5fff0217865adce2e0df4a41e1bd68d
Tomás Ferreira <ttomaz.200@gmail.com>
2017-11-13 18:42:30 +0000

    adicionados módulos para preparar a implementação de autenticaçao

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
+//authentication dependencies
+var cookieParser = require('cookie-parser');
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+//var users = require('./Server/users');
+
 //routes and API
 require('./Server/routes')(app);
 require('./Server/apiOrgs')(app);
@@ -23,8 +33,38 @@ app.set('view engine', 'pug');
 app.set('views', __dirname);
 
 //standard
-app.use(express.static(__dirname + '/Client'));
+app.use(express.static(__dirname + '/Client/Base'));
 app.use("/styles", express.static(__dirname + '/Styles'));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
 
 //Starts and listens
 var port = process.env.PORT || 3000;
