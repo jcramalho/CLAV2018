645273ad90e6238bb06652dfc40335889cbcafb6
Tomás Ferreira <ttomaz.200@gmail.com>
2018-01-23 23:33:49 +0000

    index.vue

---
+++ public/javascripts/index.vue
@@ -0,0 +1,45 @@
+new Vue({
+    el: '#acordeao',
+    components:{
+        accordion: VueStrap.accordion,
+        panel: VueStrap.panel
+    }
+})
+
+new Vue({
+    el: '#navegacao',
+    data: {
+        linhas: [
+            {
+                valor: 'Lista Consolidada',
+                consultar: '/classes',
+                submeter: '/classes/adicionar',
+            },
+            {
+                valor: 'Tabelas de Seleção',
+                consultar: '/tabelasSelecao',
+                submeter: '/tabelasSelecao/adicionar',
+            },
+            {
+                valor: 'Autos de Eliminação (WIP)',
+                consultar: '#',
+                submeter: '#',
+            },
+            {
+                valor: 'Entidades',
+                consultar: '/organizacoes',
+                submeter: '/organizacoes/adicionar',
+            },
+            {
+                valor: 'Legislação',
+                consultar: '/legislacao',
+                submeter: '/legislacao/adicionar',
+            },
+            {
+                valor: 'Estatísticas',
+                consultar: '#',
+                submeter: '#',
+            },
+        ]
+    }
+})
