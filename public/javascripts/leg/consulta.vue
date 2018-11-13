7f629bd2a31645647eb0cb3c223e2e0a5288440a
Alfredogomes <alfredommgomes@gmail.com>
2018-11-13 17:09:50 +0000

    Consulta da legislação completa

--- public/javascripts/leg/consulta.vue
+++ public/javascripts/leg/consulta.vue
@@ -2,34 +2,10 @@ var leg = new Vue({
     el: '#legislacao-form',
     data: {
         id: "",
-        legData : {
-            date: {
-                label: "Data",
-                original: "",
-            },
-            number: {
-                label: "Número",
-                original: "",
-            },
-            type: {
-                label: "Tipo",
-                original: "",
-            },
-            title: {
-                label: "Sumário",
-                original: "",
-            },
-            link: {
-                label: "Link",
-                original: "",
-            },
-            org: {
-                label: "",
-                original: [],
-            }
-        },
-        content: [],
 
+        myLegislacao: {},
+
+        entidades: "",
         ready: false,
 
         processes: [],
@@ -43,16 +19,10 @@ var leg = new Vue({
         modal: VueStrap.modal,
     },
     methods: {
-        loadProcesses: function () {
-            var classesToParse = [];
-            var keys = ["id", "Code", "Title"];
-
+        loadProcesses: function () {  
             this.$http.get("/api/legislacao/" + this.id+"/regula")
                 .then(function (response) {
-                    classesToParse = response.body;
-                })
-                .then(function () {
-                    this.processes = JSON.parse(JSON.stringify(this.parseList(classesToParse, keys)));
+                    this.processes = response.body;
 
                     if(this.processes.length>0)
                         this.processesReady = true;
@@ -61,61 +31,24 @@ var leg = new Vue({
                     console.error(error);
                 });
         },
-        parseList: function (content, keys) {
-            var dest = [];
-            var temp = {};
-
-            // parsing the JSON
-            for (var i = 0; i < content.length; i++) {
-                for (var j = 0; j < keys.length; j++) {
-                    temp[keys[j]] = content[i][keys[j]].value;
-
-                    if (keys[j] == "id") {
-                        temp.id = temp.id.replace(/[^#]+#(.*)/, '$1'); // Retira os prefixos RDF
-                    }
-                }
-
-                dest[i] = JSON.parse(JSON.stringify(temp));
-            }
-
-            return dest.sort(function (a, b) {
-                return a.id.localeCompare(b.id);
-            });
-        },   
-        parse: function(){    
-            this.legData.date.original=this.content[0].Data.value;
-            this.legData.number.original=this.content[0].Número.value;
-            this.legData.type.original=this.content[0].Tipo.value;
-            this.legData.title.original=this.content[0].Titulo.value;
-            this.legData.link.original=this.content[0].Link.value;
-
-            let ent = this.content[0].Entidades.value;
+        parse: function(){
+            //Se tiver entidades associadas colocas todas numa string separadas por espaço    
+            let ent = this.myLegislacao.entidades;
 
             if(ent.length>0){
-                this.legData.org.original = ent.split(";").map(
-                    function(e){
-                        let data = e.split("::");
-                        return {
-                            id: data[0].replace(/[^#]+#(.*)/, '$1'),
-                            sigla: data[1],
-                            nome: data[2]
-                        }
-                    }
-                );
-                for(l in this.legData.org.original){
-                    this.legData.org.label += " " + this.legData.org.original[l].sigla;
+                for(var i=0; i< ent.length; i++){
+                    this.entidades = this.entidades + " " + ent[i].entSigla;
                 }
-            }
-        
+            }      
             this.ready=true;
-        },
+        }
     },
     created: function(){
-        this.id=window.location.pathname.split('/')[3];
+        this.id=window.location.pathname.split('/')[2];
 
         this.$http.get("/api/legislacao/"+this.id)
         .then( function(response) { 
-            this.content = response.body;
+            this.myLegislacao = response.body;
         })
         .then( function() {
             this.parse();
