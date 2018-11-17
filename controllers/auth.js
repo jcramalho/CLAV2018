b68974f2f99000f98cd5cbf989881c136e69951f
Octávio Maia <octaviojmaia@gmail.com>
2018-11-17 11:21:42 +0000

    Adicionada lógica niveis de permissões.

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,14 +13,15 @@ Auth.isLoggedIn = function (req, res, next) {
                 res.redirect('/users/login');
             }else{
                 return next();
-            }  
-        }); 
+            }
+        });
     }else{
         req.flash('warn_msg', 'Login necessário para aceder a esta página');
         res.redirect('/users/login');
     }
 }
 
+/*
 Auth.isInternal = function (req, res, next) {
     if (req.isAuthenticated()) {
         if (req.user.internal) {
@@ -52,30 +53,45 @@ Auth.isExternal = function (req, res, next) {
         res.redirect('/users/login');
     }
 }
+*/
+
+Auth.checkLevel1 = function (req, res, next) {
+    return Auth.isLevel(1, req, res, next);
+}
 
 Auth.checkLevel2 = function (req, res, next) {
-    return isLevel(2, req, res, next);
+    return Auth.isLevel(2, req, res, next);
 }
 
 Auth.checkLevel3 = function (req, res, next) {
-    return isLevel(3, req, res, next);
+    return Auth.isLevel(3, req, res, next);
 }
 
 Auth.checkLevel4 = function (req, res, next) {
-    return isLevel(4, req, res, next);
+    return Auth.isLevel(4, req, res, next);
+}
+
+Auth.checkLevel5 = function (req, res, next) {
+    return Auth.isLevel(5, req, res, next);
+}
+
+Auth.checkLevel6 = function (req, res, next) {
+    return Auth.isLevel(6, req, res, next);
+}
+
+Auth.checkLevel7 = function (req, res, next) {
+    return Auth.isLevel(7, req, res, next);
 }
 
 Auth.isLevel = function (clearance, req, res, next) {
     if (req.isAuthenticated()) {
         if (req.user.level >= clearance) {
             return next();
-        }
-        else {
+        } else {
             req.flash('warn_msg', 'Não tem permissões suficientes para aceder a esta página');
-            res.redirect('back');
+            res.redirect('/');
         }
-    }
-    else {
+    } else {
         req.flash('warn_msg', 'Login necessário para aceder a esta página');
         res.redirect('/users/login');
     }
