6d980a08f86f7e842517adc555014d9559ecfc9c
Octávio Maia <octaviojmaia@gmail.com>
2018-11-26 16:18:57 +0000

    Bug fix na logica de alterar o nivel de user
    Caso nao se selecionasse opção ficava sem nivel,agora fica como nivel que estava previamente

--- public/javascripts/users/editar.vue
+++ public/javascripts/users/editar.vue
@@ -9,45 +9,48 @@ var perfil = new Vue({
     },
     methods: {
         update: function () {
-            console.log(this.selected)
-            switch(this.selected) {
-                case 'Administrador de Perfil Tecnológico':
-                    this.level = 7;
-                    break;
-                case 'Administrador de Perfil Funcional':
-                    this.level = 6;
-                    break;
-                case 'Utilizador Validador':
-                    this.level = 5;
-                    break;
-                case 'Utilizador Avançado':
-                    this.level = 4;
-                    break;
-                case 'Utilizador Decisor':
-                    this.level = 3;
-                    break;
-                case 'Utilizador Simples':
-                    this.level = 2;
-                    break;
-                case 'Representante Entidade':
-                    this.level = 1;
-                    break;
-            }
-
-            var dataObj = {
-                Level: this.level,
-                id: window.location.pathname.split('/')[3],
-            }
-
-            this.$http.post('/users/updateLevel', dataObj, {
-                headers: {
-                    'content-type': 'application/json'
+            if (!this.edit.Level) {
+                return false;
+            }else{
+                switch(this.selected) {
+                    case 'Administrador de Perfil Tecnológico':
+                        this.level = 7;
+                        break;
+                    case 'Administrador de Perfil Funcional':
+                        this.level = 6;
+                        break;
+                    case 'Utilizador Validador':
+                        this.level = 5;
+                        break;
+                    case 'Utilizador Avançado':
+                        this.level = 4;
+                        break;
+                    case 'Utilizador Decisor':
+                        this.level = 3;
+                        break;
+                    case 'Utilizador Simples':
+                        this.level = 2;
+                        break;
+                    case 'Representante Entidade':
+                        this.level = 1;
+                        break;
                 }
-            }).then(function (response) {
-
-            }).catch(function (error) {
-                console.error(error);
-            });
+    
+                var dataObj = {
+                    Level: this.level,
+                    id: window.location.pathname.split('/')[3],
+                }
+    
+                this.$http.post('/users/updateLevel', dataObj, {
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                }).then(function (response) {
+    
+                }).catch(function (error) {
+                    console.error(error);
+                });
+            }
         },
     },
 })
