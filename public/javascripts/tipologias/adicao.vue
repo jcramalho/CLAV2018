103e4128b2fb609709498122c7430cbf7a979718
Alfredogomes <alfredommgomes@gmail.com>
2018-12-06 11:16:47 +0000

    Na adição de Tipologias adicionei o campo "Elementos" falta adicionar os campos das classes

--- public/javascripts/tipologias/adicao.vue
+++ public/javascripts/tipologias/adicao.vue
@@ -1,20 +1,63 @@
 var newOrg = new Vue({
     el: '#nova-entidade-form',
     data: {
-        des: "",
-        sigla: "",
+        tipologia: {
+            des: "",
+            sigla: "",
+        },
         message: "",
+        entidades: [],
+        ent: [],
+        entReady: false,
+        entTableHeader: ["#", "Sigla", "Nome", "Tipo"],
+        entTableWidth: ["4%", "15%", "70%", "15%"],
     },
     components: {
-        spinner: VueStrap.spinner
+        spinner: VueStrap.spinner,
+        accordion: VueStrap.accordion,
+        panel: VueStrap.panel,
+        modal: VueStrap.modal,
     },
     methods: {
+        loadEnts: function () {
+            var i = 0;
+
+            this.$http.get("/api/entidades")
+                .then(function (response) {
+                    this.ent = response.body
+                        .map(function (item) {
+                            return {
+                                data: [i++, item.sigla, item.designacao, "Entidade"],
+                                selected: false,
+                                id: item.id
+                            }
+                        }).sort(function (a, b) {
+                            return a.data[1].localeCompare(b.data[1]);
+                        });
+
+                    this.entReady = true;
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });       
+        },
+        entSelected: function (row, list, partType) {
+            if (!row.selected) {
+                list.push(row.id);
+            }
+            else {
+                let index = list.indexOf(row.id);
+                if (index != -1) {
+                    list.splice(index, 1);
+                }
+            }
+        },
         add: function () {
             this.$refs.spinner.show();
 
             var dataObj = {
-                des: this.des,
-                sigla: this.sigla,
+                des: this.tipologia.des,
+                sigla: this.tipologia.sigla,
             }
 
             this.$http.post('/api/tipologias/', dataObj, {
@@ -36,5 +79,8 @@ var newOrg = new Vue({
                     console.error(error);
                 });
         }
+    },
+    created: function () {
+        this.loadEnts();
     }
 })
