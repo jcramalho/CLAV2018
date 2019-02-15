cb8b06111e2b06e9b4a04da14df3d707c2ca1c72
José Carlos Ramalho <jcr@di.uminho.pt>
2019-02-15 15:18:50 +0000

    Retoques no classe-get.pug

---
+++ public/javascripts/v-components/top-menu-bar.js
@@ -0,0 +1,40 @@
+<template>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Classes</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" 
+                href="#" role="button" aria-haspopup="true" aria-expanded="false">Classes</a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Criar</a>
+                <a class="dropdown-item" href="#">Consultar</a>
+                <a class="dropdown-item" href="#">Alterar</a>
+                <a class="dropdown-item" href="#">Desativar</a>
+                <a class="dropdown-item" href="#">Listar</a>
+            </div>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" 
+                href="#" role="button" aria-haspopup="true" aria-expanded="false">Entidades</a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Criar</a>
+                <a class="dropdown-item" href="#">Consultar</a>
+                <a class="dropdown-item" href="#">Alterar</a>
+                <a class="dropdown-item" href="#">Desativar</a>
+                <a class="dropdown-item" href="#">Listar</a>
+            </div>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" 
+                href="#" role="button" aria-haspopup="true" aria-expanded="false">Legislação</a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Criar</a>
+                <a class="dropdown-item" href="#">Consultar</a>
+                <a class="dropdown-item" href="#">Alterar</a>
+                <a class="dropdown-item" href="#">Desativar</a>
+                <a class="dropdown-item" href="#">Listar</a>
+            </div>
+        </li>
+    </ul>
+</template>
