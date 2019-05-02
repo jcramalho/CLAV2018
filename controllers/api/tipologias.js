612bc47b0dbe078a3b82cee14ccb462900663373
José Carlos Ramalho <jcr@di.uminho.pt>
2019-05-02 20:13:57 +0100

    Corrigi o bug nas tipologias como dono.

--- controllers/api/tipologias.js
+++ controllers/api/tipologias.js
@@ -172,7 +172,7 @@ Tipologias.elementos = (id) => {
  */
 Tipologias.dono = (id) => {
     const query = `SELECT ?id ?codigo ?titulo WHERE {
-        ?uri clav:temDono clav:tip_AP ;
+        ?uri clav:temDono clav:${id} ;
             clav:codigo ?codigo ;
             clav:titulo ?titulo ;
             clav:pertenceLC clav:lc1 ;
