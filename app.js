7e12bfdbe8c1ee1a7d2d61525d5289e27e43c0a4
Jose Carlos Ramalho <jcr@di.uminho.pt>
2019-05-22 10:21:00 +0100

    Limpei a configuração do CORS, tinha duplicados...

--- app.js
+++ app.js
@@ -7,11 +7,9 @@ var logger = require('morgan')
 
 // Para permitir pedidos à API vindos de outros serviços internos
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Origin, Accept');
     next();
 });
 
