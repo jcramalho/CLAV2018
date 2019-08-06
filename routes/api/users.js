e68513715ee32126e25f40195545dc9618deb186
Octávio Maia <octaviojmaia@gmail.com>
2019-08-06 19:46:28 +0100

    Bug fix: Quando registava users sem ser pelo CC a app crashava

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,7 @@ var Users = require('../../controllers/api/users');
 var jwt = require('jsonwebtoken');
 var secretKey = require('./../../config/app');
 var Mailer = require('../../controllers/api/mailer');
+var mongoose = require('mongoose');
 
 router.get('/', (req, res) => {
     Users.listar(req,function(err, result){
@@ -57,6 +58,7 @@ router.get('/verificaToken/:id', async function(req,res){
 router.post('/registar', function (req, res) {
     var internal = (req.body.type > 1);
     var newUser = new User({
+        _id: mongoose.Types.ObjectId(),
         name: req.body.name,
         email: req.body.email,
         entidade: 'ent_'+req.body.entidade,
@@ -67,11 +69,11 @@ router.post('/registar', function (req, res) {
         }
     });
     
-    Users.getUserByEmail(req.body.email, function (err, user) {
+    Users.getUserByEmail(req.body.email, async function (err, user) {
         if (err) 
             return res.status(500).send(`Erro: ${err}`);
         if (!user) {
-            Users.createUser(newUser, function (err, user) {
+            await Users.createUser(newUser, function (err, user) {
                 Logging.logger.info('Utilizador ' + user._id + ' registado.');
                 if (err) return res.status(500).send(`Erro: ${err}`);
             });
@@ -109,9 +111,13 @@ router.post('/recuperar', function (req, res) {
         if (!user)
             res.send('Não existe utilizador com esse email!');
         else {
-            var token = jwt.sign({id: user._id}, secretKey.key, {expiresIn: '30m'});
-            Mailer.sendEmailRecuperacao(req.body.email, req.body.url.split('/recuperacao')[0]+'/alteracaoPassword?jwt='+token);
-            res.send('Email enviado com sucesso!')
+            if(user._id.toString().includes('ObjectId')){
+                var token = jwt.sign({id: user._id}, secretKey.key, {expiresIn: '30m'});
+                Mailer.sendEmailRecuperacao(req.body.email, req.body.url.split('/recuperacao')[0]+'/alteracaoPassword?jwt='+token);
+                res.send('Email enviado com sucesso!')
+            }else{
+                res.send('Este utilizador foi registado através do Cartão de Cidadão!')
+            }
         }
     });
 });
@@ -234,7 +240,7 @@ router.post('/callback', async function(req, res){
             Users.getUserById(NIC, function (err, user) {
                 if(!user){ //Este cartao cidadao nao tem user associado
                     res.writeHead(301,{
-                        Location: 'http://localhost:8080/users/HandlerCC?NIC='+result.NIC+'&Nome='+result.NomeCompleto
+                        Location: 'http://localhost:8080/users/HandlerCC?NIC='+result.NIC+'&Nome='+result.NomeCompleto //TODO depois temos de mudar de localhost para o URL final
                     });
                     res.end();
                 }else{ //ja existe user com este cartao cidadao
