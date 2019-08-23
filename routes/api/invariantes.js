1c974a2bea7879377feb38129c25d864de717965
jcm300 <zeclmartins@gmail.com>
2019-08-23 17:05:10 +0100

    Adicionada a possibilidade de testar todos os invariantes

--- routes/api/invariantes.js
+++ routes/api/invariantes.js
@@ -3,6 +3,13 @@ var Invariantes = require('../../controllers/api/invariantes.js');
 var express = require('express');
 var router = express.Router();
 
+//devolve os erros de todos os invariantes, devolve apenas os invariantes que possuem erros
+router.get('/testarTodos', async function (req, res){
+    Invariantes.getTodosErros()
+        .then(dados => res.jsonp(dados))
+        .catch(erro => res.status(500).jsonp(`Erro na obtenção de todos os erros: ${erro}`))
+})
+
 //devolve a lista com todos os invariantes caso não seja fornecido os ids (idRel e idInv). No caso de os ids serem fornecidos devolve os erros resultantes desse invariante
 router.get('/', async function (req, res) {
 
