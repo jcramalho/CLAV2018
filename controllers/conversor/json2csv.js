9898fa43e918903cccb55ed7514a0d2b4ea9cdd4
jcm300 <zeclmartins@gmail.com>
2019-09-02 17:23:11 +0100

    Pequenas correções no conversor de json para csv

--- controllers/conversor/json2csv.js
+++ controllers/conversor/json2csv.js
@@ -10,69 +10,72 @@ function json2csvArray(name, array){
     if(len){
         if(array[0] instanceof Array){
             var header = name != '' ? name + '.' : ''
-            array.forEach((arr, index) => accum = accum.concat(json2csvRec(header + index, arr)))
-        }else if(typeof array[0] === 'object'){
-            //headers
-            for(var key in array[0]){
-                var header = name != '' ? name + '.' + key : key
-                csv[0].push(protect(header))
-            }
-            //lines
-            for(var i=0; i<len; i++){
-                var line = []
-                for(var key in array[i]){
-                    var header = name != '' ? name + '.' : ''
-
-                    if(typeof array[i][key] != 'object' && !(array[i][key] instanceof Array)){
-                        line.push(protect(array[i][key]))
-                    }else{
-                        accum = accum.concat(json2csvRec(header + key + '.' + i, array[i][key]))
-                    }
-                }
-                csv.push(line)
-            }
+            array.forEach((arr, index) => csv = csv.concat(json2csvRec(header + index, arr)))
         }else{
-            csv[0].push(protect(name))
-            array.forEach(e => csv.push([e]))
+            if(typeof array[0] === 'object'){
+
+                //headers
+                for(var key in array[0]){
+                    var header = name != '' ? name + '.' + key : key
+                    csv[0].push(protect(header))
+                }
+                //lines
+                for(var i=0; i<len; i++){
+                    var line = []
+                    for(var key in array[i]){
+                        var header = name != '' ? name + '.' : ''
+
+                        if(typeof array[i][key] != 'object' && !(array[i][key] instanceof Array)){
+                            line.push(protect(array[i][key]))
+                        }else{
+                            accum = accum.concat(json2csvRec(header + key + '.' + i, array[i][key]))
+                        }
+                    }
+
+                    csv.push(line)
+                }
+            }else{
+                csv[0].push(protect(name))
+                array.forEach(e => csv.push([e]))
+            }
         }
+    }else{
+        csv[0].push(protect(name))
     }
 
-    if(csv[0].length == 0){
-        csv = csv.slice(1)
-    }
     csv.push([])
     csv = csv.concat(accum)
     return csv
 }
 
 function json2csvRec(name,json){
-    var csvLines = [[],[],[]]
+    var csvLines
 
     if(json instanceof Array){
-        csvLines = csvLines.concat(json2csvArray(name, json))
+        csvLines = json2csvArray(name, json)
     }else{
+        csvLines = [[],[],[]]
+
         for(var key in json){
             var header = name != '' ? name + '.' + key : key
 
             if(json[key] instanceof Array){
                 csvLines = csvLines.concat(json2csvArray(header, json[key]))
             }else{
-                var type = typeof json[key]
-
-                if(type === 'object' || type === 'function'){
-                    var l = json2csvRec(header, json[key])
-                    csvLines = csvLines.concat(l)
+                if(typeof json[key] === 'object'){
+                    csvLines = csvLines.concat(json2csvRec(header, json[key]))
                 }else{
                     csvLines[0].push(protect(header))
                     csvLines[1].push(protect(json[key]))
                 }
             }
         }
+
+        if(csvLines[0].length == 0 && csvLines[1].length == 0){
+            csvLines = csvLines.slice(3)
+        }
     }
     
-    if(csvLines[0].length == 0 && csvLines[1].length == 0){
-        csvLines = csvLines.slice(3)
-    }
     return csvLines
 }
 
