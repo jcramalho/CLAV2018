dc83f549153a86c09cbea61992f9160fc43bb13e
jcm300 <zeclmartins@gmail.com>
2019-09-07 13:41:52 +0100

    Mudada a rota de /api/ontologia/exportar para /api/ontologia

--- routes/api/ontologia.js
+++ routes/api/ontologia.js
@@ -4,6 +4,17 @@ var Ontologia = require('../../controllers/api/ontologia.js');
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res) => {
+    var format = req.query.format || req.headers.accept
+    Ontologia.exportar(req.query.infer, format, 7)
+        .then(dados => {
+            res.setHeader("content-disposition","attachment; filename=clav." + dados[2])
+            res.setHeader('content-type', dados[1])
+            res.send(dados[0])
+        })
+        .catch(erro => res.status(500).send(`Erro: ${erro}`))
+})
+
 router.get('/data', (req, res) => {
     Ontologia.data()
         .then(dados => res.json(dados))
@@ -14,17 +25,6 @@ router.get('/descricao', (req, res) => {
     Ontologia.descricao()
         .then(dados => res.json(dados))
         .catch(erro => res.status(500).send(`Erro ao obter data da ontologia: ${erro}`))
-})
-
-router.get('/exportar', (req, res) => {
-    var format = req.query.format || req.headers.accept
-    Ontologia.exportar(req.query.infer, format, 7)
-        .then(dados => {
-            res.setHeader("content-disposition","attachment; filename=clav." + dados[2])
-            res.setHeader('content-type', dados[1])
-            res.send(dados[0])
-        })
-        .catch(erro => res.status(500).send(`Erro: ${erro}`))
 })
 
 module.exports = router;
