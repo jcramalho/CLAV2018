b189a7c42b3f439aad65910d0f82b86451e2712b
José Carlos Ramalho <jcr@endpriv087.scom-glt.uminho.pt>
2019-09-24 22:19:16 +0100

    Corrigi bug na rota /api/notasAp.

--- routes/api/notasAp.js
+++ routes/api/notasAp.js
@@ -6,7 +6,7 @@ var express = require('express');
 var router = express.Router();
 
 // Devolve a lista de todas as notas de aplicação: idNota, nota, codigoProc, tituloProc
-router.get('/notasAp', (req, res) => {
+router.get('/', (req, res) => {
     NotasAp.todasNotasAp()
         .then(dados => res.jsonp(dados))
         .catch(erro => res.status(500).send(`Erro na recuperação total das notas de aplicação: ${erro}`))
