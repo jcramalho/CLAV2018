bcb258c9e72559555e661422507d5ac51a085d4e
José Carlos Ramalho <jcr@endpriv087.scom-glt.uminho.pt>
2019-09-24 22:37:22 +0100

    Acrescentei a rota /api/exemplosNotasAp

---
+++ controllers/api/exemplosNotasAp.js
@@ -0,0 +1,16 @@
+const execQuery = require('../../controllers/api/utils').execQuery
+const normalize = require('../../controllers/api/utils').normalize;
+var ExemplosNotasAp = module.exports
+
+// Devolve a lista com todas as notas de aplicação: idNota, nota, codigoProc, tituloProc
+ExemplosNotasAp.todosExemplosNotasAp = () => {
+    var query = `
+    SELECT ?idExemplo ?exemplo ?codigoProc ?tituloProc WHERE { 
+        ?idExemplo a clav:ExemploNotaAplicacao .
+        ?idExemplo clav:conteudo ?exemplo .
+        ?codigoProc clav:temExemploNA ?idExemplo .
+        ?codigoProc clav:titulo ?tituloProc .
+    }`
+    return execQuery("query", query)
+        .then(response => normalize(response))
+}
