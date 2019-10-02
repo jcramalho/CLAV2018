8d5258f67c292d689aaf187cd357376e38319356
jcm300 <zeclmartins@gmail.com>
2019-10-02 09:21:21 +0100

    Adicionada headers cors para autenticação

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
 // Para permitir pedidos à API vindos de outros serviços internos
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Origin, Accept');
     next();
