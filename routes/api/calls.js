7230c86b74e203a268215eb186a3ef45f19a1ece
jcm300 <zeclmartins@gmail.com>
2019-10-10 19:06:09 +0100

    Simplificação e alteração do nivel de uma rota

--- routes/api/calls.js
+++ routes/api/calls.js
@@ -4,19 +4,19 @@ var Calls = require('../../controllers/api/calls.js');
 var express = require('express');
 var router = express.Router();
 
-router.get('/', Auth.isLoggedInUser, Auth.checkLevel([6, 7]), async (req, res) => {
+router.get('/', Auth.isLoggedInUser, Auth.checkLevel(6), async (req, res) => {
     Calls.getAllCalls()
         .then(data => res.jsonp(data))
         .catch (error => res.status(500).send(`Erro ao obter os logs: ${error}`))
 })
 
-router.get('/:type/:id', Auth.isLoggedInUser, Auth.checkLevel([6, 7]), async function(req,res){
+router.get('/:type/:id', Auth.isLoggedInUser, Auth.checkLevel(6), async function(req,res){
     Calls.getUserCalls(req.params.id, req.params.type)
         .then(data => res.jsonp(data))
         .catch (error => res.status(500).send(`Erro ao obter os logs de ${req.params.type} com o id ${req.params.id}: ${error}`))
 })
 
-router.get('/route/:method/:route', Auth.isLoggedInUser, Auth.checkLevel([6, 7]), async function(req,res){
+router.get('/route/:method/:route', Auth.isLoggedInUser, Auth.checkLevel(6), async function(req,res){
     console.log(decodeURIComponent(req.params.route))
     Calls.getRouteCalls(decodeURIComponent(req.params.route), req.params.method)
         .then(data => res.jsonp(data))
