d8fbdffbf82aac4910db0e9656bdd2f4f0c15ce8
ruiaraujo <ruiaraujo012@gmail.com>
2019-10-10 17:09:44 +0100

    Adição da rota para verificar a existência do termo de indice

--- controllers/api/termosIndice.js
+++ controllers/api/termosIndice.js
@@ -1,5 +1,5 @@
 const execQuery = require('../../controllers/api/utils').execQuery
-const normalize = require('../../controllers/api/utils').normalize;
+const normalize = require('../../controllers/api/utils').normalize
 var TermosIndice = module.exports
 
 /**
@@ -13,12 +13,12 @@ var TermosIndice = module.exports
 
 /**
  * Lista os termos de índice registados no sistema.
- * 
+ *
  * @return {Promise<[TermoIndice] | Error>} promessa que quando cumprida contém a
  * lista dos termos de índice
  */
 TermosIndice.listar = () => {
-    let query = `
+	let query = `
         SELECT ?id ?termo ?idClasse ?tituloClasse ?estado ?codigoClasse
         WHERE { 
             ?idTI rdf:type clav:TermoIndice ;
@@ -30,60 +30,60 @@ TermosIndice.listar = () => {
             BIND(STRAFTER(STR(?idTI), "clav#") AS ?id) .
             BIND(STRAFTER(STR(?idC), "clav#") AS ?codigoClasse)
         }
-        ORDER BY ?termo`;
+        ORDER BY ?termo`
 
-    return execQuery("query", query)
-        .then(response => normalize(response));
+	return execQuery('query', query).then((response) => normalize(response))
 }
 
 // Testa a existência de um determinado TI
 TermosIndice.existe = (termoIndice) => {
-    const query = `
+	const query = `
         ASK { 
             ?s rdf:type clav:TermoIndice.
             ?s clav:termo "${termoIndice}"
-        }`;
-    return execQuery("query", query)
-        .then(response => {return (response.boolean)});
+        }`
+	return execQuery('query', query).then((response) => {
+		return response.boolean
+	})
 }
 
-TermosIndice.assocClasse = classe => {
-    let query = `
+TermosIndice.assocClasse = (classe) => {
+	let query = `
         SELECT ?id ?termo WHERE { 
             ?idTI rdf:type clav:TermoIndice ;
                 clav:termo ?termo ;
                 clav:estaAssocClasse clav:${classe} .
             BIND (STRAFTER(STR(?idTI), 'clav#') AS ?id).
         }
-        ORDER BY ?termo`;
+        ORDER BY ?termo`
 
-    return execQuery("query", query)
-        .then(response => normalize(response));
+	return execQuery('query', query).then((response) => normalize(response))
 }
 
-TermosIndice.lastID = function () {
-    let fetchQuery = `
+TermosIndice.lastID = function() {
+	let fetchQuery = `
         SELECT * WHERE { 
             ?id rdf:type clav:TermoIndice
         } ORDER BY DESC(?id)
         LIMIT 1
-    `;
+    `
 
-    return execQuery("query", fetchQuery)
-        //getting the content we want
-        .then(response => Promise.resolve(response.results.bindings[0]))
-        .catch(function (error) {
-            console.error(error);
-        });
+	return (
+		execQuery('query', fetchQuery)
+			//getting the content we want
+			.then((response) => Promise.resolve(response.results.bindings[0]))
+			.catch(function(error) {
+				console.error(error)
+			})
+	)
 }
 
 TermosIndice.contar = function() {
-    let query = `
+	let query = `
         SELECT (count (?s) as ?num) WHERE { 
             ?s rdf:type clav:TermoIndice
         }
-    `;
+    `
 
-    return execQuery("query", query)
-        .then(response => normalize(response));
+	return execQuery('query', query).then((response) => normalize(response))
 }
