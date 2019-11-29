33a47f0628400760464fb795605d4268e66cb65b
jcm300 <zeclmartins@gmail.com>
2019-11-29 19:50:59 +0000

    Agora na importação de uma TS não é necessário indicar email nem entidade do user

--- routes/api/tabsSel.js
+++ routes/api/tabsSel.js
@@ -1,5 +1,6 @@
 var Auth = require('../../controllers/auth.js');
 var SelTabs = require('../../controllers/api/tabsSel.js');
+var Users = require('../../controllers/api/users.js');
 
 const Excel = require('exceljs/modern.nodejs');
 var formidable = require("formidable")
@@ -48,101 +49,78 @@ router.get('/:id', Auth.isLoggedInKey, function (req, res) {
 
 router.post('/CSV', Auth.isLoggedInUser, Auth.checkLevel([1, 3, 3.5, 4, 5, 6, 7]), async function (req, res){
     var form = new formidable.IncomingForm()
-
-    form.parse(req, async (error, fields, formData) => {
-        if(!error){
-            if(formData.file && formData.file.type && formData.file.path &&
-               fields.email &&
-               fields.entidade_user &&
-               fields.tipo_ts &&
-               (fields.entidade_ts || fields.tipo_ts == 'TS Pluriorganizacional')){
-                var workbook = new Excel.Workbook();
-
-                if(!fields.entidade_ts){
-                    fields.entidade_ts = null
-                }
-
-                if(formData.file.type == "text/csv"){
-                    var possibleDelimiters = [",",";","\t","|"]
-                    var i = 0
-                    var len = possibleDelimiters.length
-                    var parsed = false
-
-                    var options = {
-                        //Substitui função por forma a não realizar parse de datas, igual a usada pelo exceljs, caso vazio devolve null, no resto devolve string
-                        map: (value, index) => {
-                            if (value === '') {
-                                return null;
-                            }
-                            
-                            //const datumNumber = Number(value);
-                            //if (!Number.isNaN(datumNumber)) {
-                            //    return datumNumber;
-                            //}
-
-                            //Em comentário por forma a não fazer parse de datas
-                            //para usar dayjs fazer: const dayjs = require('dayjs');
-                            //const dt = dayjs(value, dateFormats, true);
-                            //if (dt.isValid()) {
-                            //    return new Date(dt.valueOf());
-                            //}
-
-                            //const SpecialValues = {
-                            //    true: true,
-                            //    false: false,
-                            //    '#N/A': {error: '#N/A'},
-                            //    '#REF!': {error: '#REF!'},
-                            //    '#NAME?': {error: '#NAME?'},
-                            //    '#DIV/0!': {error: '#DIV/0!'},
-                            //    '#NULL!': {error: '#NULL!'},
-                            //    '#VALUE!': {error: '#VALUE!'},
-                            //    '#NUM!': {error: '#NUM!'},
-                            //}
-
-                            //const special = SpecialValues[value];
-                            //if (special !== undefined) {
-                            //    return special;
-                            //}
-                            
-                            return value;
-                        }
-                    }
-
-                    while(!parsed){
-                        try{
-                            //tenta as várias hipóteses de delimitadores do CSV
-                            options.delimiter = possibleDelimiters[i]
-                            var worksheet = await workbook.csv.readFile(formData.file.path, options)
-                            parsed = true
-                        }catch(erro){
-                            if(++i == len){
-                                parsed = true
-                                res.status(500).json(`Erro ao importar CSV: Não foi possível fazer parsing do ficheiro.\nOs delimitadores podem ser: , ou ; ou \\t ou |.\nPara além disso o quote e o escape são realizados através de ".\nPor fim, o encoding do ficheiro tem de ser UTF-8.`)
-                            }
-                        }
-                    }
-
-                    if(i < len){
-                        SelTabs.criarPedidoDoCSV(workbook, fields.email, fields.entidade_user, fields.entidade_ts, fields.tipo_ts)
-                            .then(codigoPedido => res.json(codigoPedido))
-                            .catch(erro => res.status(500).json(`Erro ao importar CSV: ${erro}`))
-                    }
-                }else if(formData.file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"){
-                    workbook.xlsx.readFile(formData.file.path)
-                        .then(function() {
-                            SelTabs.criarPedidoDoCSV(workbook, fields.email, fields.entidade_user, fields.entidade_ts, fields.tipo_ts)
-                                .then(dados => res.json(dados))
-                                .catch(erro => res.status(500).json(`Erro ao importar Excel: ${erro}`))
-                        })
-                        .catch(erro => res.status(500).json(`Erro ao importar Excel: ${erro}`))
-                }else{
-                    res.status(415).json(`Erro ao importar CSV/Excel: o ficheiro tem de estar no formato CSV ou Excel (.xlsx)`)
-                }
-            }else{
-                res.status(500).json(`Erro ao importar CSV/Excel: O FormData deve possuir quatro campos: um ficheiro em file, um email em email, a sigla da entidade do utilizador em entidade_user e o tipo de TS em tipo_ts. Caso o tipo de TS seja 'TS Organizacional' deve possuir outro campo: a sigla da entidade da TS em entidade_ts.`)
-            }
+    
+    Users.getUserById(req.user.id, function(err, user) {
+        if(err){
+            res.status(500).json(`Erro ao importar CSV/Excel, não foi possível obter o utilizador: ${err}`)
         }else{
-            res.status(500).json(`Erro ao importar CSV/Excel: ${error}`)
+            var entidade_user = req.user.entidade.split("_")[1]
+
+            form.parse(req, async (error, fields, formData) => {
+                if(!error){
+                    if(formData.file && formData.file.type &&
+                       formData.file.path && fields.tipo_ts &&
+                       (fields.entidade_ts || fields.tipo_ts == 'TS Pluriorganizacional')){
+                        var workbook = new Excel.Workbook();
+
+                        if(!fields.entidade_ts){
+                            fields.entidade_ts = null
+                        }
+
+                        if(formData.file.type == "text/csv"){
+                            var possibleDelimiters = [",",";","\t","|"]
+                            var i = 0
+                            var len = possibleDelimiters.length
+                            var parsed = false
+
+                            var options = {
+                                //Substitui função por forma a não realizar parse de datas, igual a usada pelo exceljs, caso vazio devolve null, no resto devolve string
+                                map: (value, index) => {
+                                    if (value === '') {
+                                        return null;
+                                    }
+
+                                    return value;
+                                }
+                            }
+
+                            while(!parsed){
+                                try{
+                                    //tenta as várias hipóteses de delimitadores do CSV
+                                    options.delimiter = possibleDelimiters[i]
+                                    var worksheet = await workbook.csv.readFile(formData.file.path, options)
+                                    parsed = true
+                                }catch(erro){
+                                    if(++i == len){
+                                        parsed = true
+                                        res.status(500).json(`Erro ao importar CSV: Não foi possível fazer parsing do ficheiro.\nOs delimitadores podem ser: , ou ; ou \\t ou |.\nPara além disso o quote e o escape são realizados através de ".\nPor fim, o encoding do ficheiro tem de ser UTF-8.`)
+                                    }
+                                }
+                            }
+
+                            if(i < len){
+                                SelTabs.criarPedidoDoCSV(workbook, user.email, entidade_user, fields.entidade_ts, fields.tipo_ts)
+                                    .then(codigoPedido => res.json(codigoPedido))
+                                    .catch(erro => res.status(500).json(`Erro ao importar CSV: ${erro}`))
+                            }
+                        }else if(formData.file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"){
+                            workbook.xlsx.readFile(formData.file.path)
+                                .then(function() {
+                                    SelTabs.criarPedidoDoCSV(workbook, user.email, entidade_user, fields.entidade_ts, fields.tipo_ts)
+                                        .then(dados => res.json(dados))
+                                        .catch(erro => res.status(500).json(`Erro ao importar Excel: ${erro}`))
+                                })
+                                .catch(erro => res.status(500).json(`Erro ao importar Excel: ${erro}`))
+                        }else{
+                            res.status(415).json(`Erro ao importar CSV/Excel: o ficheiro tem de estar no formato CSV ou Excel (.xlsx)`)
+                        }
+                    }else{
+                        res.status(500).json(`Erro ao importar CSV/Excel: O FormData deve possuir quatro campos: um ficheiro em file, um email em email, a sigla da entidade do utilizador em entidade_user e o tipo de TS em tipo_ts. Caso o tipo de TS seja 'TS Organizacional' deve possuir outro campo: a sigla da entidade da TS em entidade_ts.`)
+                    }
+                }else{
+                    res.status(500).json(`Erro ao importar CSV/Excel: ${error}`)
+                }
+            })
         }
     })
 })
