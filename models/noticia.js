ce95bbe18406e1054d181ac5b0ff7629c168a05f
José Carlos Ramalho <jcr@iMac-de-iMac.lan>
2019-12-19 15:57:26 +0000

    Acrescentei mais um invariante sobre a existência de justificação no PCA.

---
+++ models/noticia.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const NoticiaSchema = new mongoose.Schema({
+    data: {
+        type: String,
+        required: true
+    },
+    titulo: {
+        type: String,           
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Noticia', NoticiaSchema, 'noticias');
