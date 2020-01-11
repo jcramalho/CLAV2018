3b5e1cb8b56318710f432a6fbf66dd8b055d6131
jcm300 <zeclmartins@gmail.com>
2020-01-11 14:26:11 +0000

    Melhorias do código de exportação de CSV

--- controllers/conversor/json2csv.js
+++ controllers/conversor/json2csv.js
@@ -1,329 +1,230 @@
-var separator = ';'
+//CSV separator to use
+const separator = ';'
+//Entity types that is possible to convert to CSV
+const types = ["classes", "classe", "entidades", "entidade", "tipologias", "tipologia",  "legislacoes", "legislacao"]
+//How is converted each field of each entities, format:
+// entity : {
+//      field: [title_to_use, func_to_aplicate to field value],
+//      ...
+// }
+const convert_to = {
+    "classe": {
+        "codigo": ["Código", v => v],
+        "titulo": ["Título", v => v],
+        "descricao": ["Descrição", v => v],
+        "notasAp": ["Notas de aplicação", map_value("nota")],
+        "exemplosNotasAp": ["Exemplos de NA", map_value("exemplo")],
+        "notasEx": ["Notas de exclusão", map_value("nota")],
+        "termosInd": ["Termos Indice", map_value("termo")],
+        "tipoProc": ["Tipo de processo", v => v],
+        "procTrans": ["Processo transversal (S/N)", v => v],
+        "dono": ["Dono", v => v], //caso do esqueleto
+        "participante": ["Participante", v => v], //caso do esqueleto
+        "donos": ["Donos do processo", map_value("sigla")],
+        "participantes_sigla": ["Participante no processo", map_value("sigla")],
+        "participantes_participLabel": ["Tipo de intervenção do participante", map_value("participLabel")],
+        "processosRelacionados_codigo": ["Código do processo relacionado", map_value("codigo")],
+        "processosRelacionados_titulo": ["Título do processo relacionado", map_value("titulo")],
+        "processosRelacionados_idRel": ["Tipo de relação entre processos", map_value("idRel")],
+        "legislacao_idLeg": ["Diplomas jurídico-administrativos REF Ids", map_value("idLeg")],
+        "legislacao_titulos": ["Diplomas jurídico-administrativos REF Títulos", leg_titulos],
+        "pca": ["", pca_df("pca")],
+        "df": ["", pca_df("df")],
+        "filhos": [null, filhos]
+    },
+    "entidade": {
+        "sigla": ["Sigla", v => v],
+        "designacao": ["Designação", v => v],
+        "estado": ["Estado", v => v],
+        "sioe": ["ID SIOE", v => v],
+        "internacional": ["Internacional", internacional],
+        "dono": ["Dono no processo", map_value("codigo")],
+        "participante_codigo": ["Participante no processo", map_value("codigo")],
+        "participante_tipoPar": ["Tipo de intervenção no processo", map_value("tipoPar")],
+        "tipologias": ["Tipologias da entidade", map_value("sigla")]
+    },
+    "tipologia": {
+        "sigla": ["Sigla", v => v],
+        "designacao": ["Designação", v => v],
+        "estado": ["Estado", v => v],
+        "dono": ["Dono no processo", map_value("codigo")],
+        "participante_codigo": ["Participante no processo", map_value("codigo")],
+        "participante_tipoPar": ["Tipo de intervenção no processo", map_value("tipoPar")]
+    },
+    "legislacao": {
+        "tipo": ["Tipo", v => v],
+        "numero": ["Número", v => v],
+        "data": ["Data", v => v],
+        "sumario": ["Sumário", v => v],
+        "fonte": ["Fonte", v => v],
+        "link": ["Link", v => v],
+        "entidades": ["Entidades", entidades],
+        "regula": ["Regula processo", map_value("codigo")]
+    },
+    "pca": {
+        "valores": ["Prazo de conservação administrativa", v => v],
+        "notas": ["Nota ao PCA", v => v],
+        "formaContagem": ["Forma de contagem do PCA", v => v],
+        "subFormaContagem": ["Sub Forma de contagem do PCA", v => v],
+        "justificacao_criterio": ["Critério PCA", map_value("tipoId")],
+        "justificacao_refs": ["ProcRefs/LegRefs PCA", refs]
+    },
+    "df": {
+        "valor": ["Destino final", destino_final],
+        "notas": ["Notas ao DF", v => v],
+        "justificacao_criterio": ["Critério DF", map_value("tipoId")],
+        "justificacao_refs": ["ProcRefs/LegRefs DF", refs]
+    }
+}
 
+//convert internacional field
+function internacional(value){
+    if(value == "")
+        return "Não"
+    else
+        return "Sim"
+}
+
+//join elements that should be in the same csv "square"
+function join(array){
+    return array.join('#\n')
+}
+
+//convert array where we only need one field from each object element
+function map_value(key){
+    return function(value){
+        return join(value.map(p => p[key]))
+    }
+}
+
+//obtain legislacao title
+function leg_titulos(value){
+    return join(value.map(l => l.tipo + ' ' + l.numero))
+}
+
+//parse entidades of legislacao, if is an array of objects we should obtain only the siglas
+function entidades(value){
+    var v = value
+
+    if(value[0] && value[0].sigla)
+        v = value.map(t => t.sigla)
+
+    return join(v)
+}
+
+//if DF is equal to "NE" should be empty
+function destino_final(value){
+    if(value == "NE")
+        value = ""
+
+    return value 
+}
+
+//get legislacao references (processos references e legislacao references)
+function refs(value){
+    var procs_legs = []
+
+    value.forEach(just => {
+        if(just.processos.length > 0)
+            procs_legs.push('(' + join(just.processos.map(p => p.procId)) + ')')
+        else if(just.legislacao.length > 0)
+            procs_legs.push('(' + join(just.legislacao.map(l => l.legId)) + ')')
+        else
+            procs_legs.push('()')
+    })
+
+    return join(procs_legs)
+}
+
+//parse PCA or DF according to the key ("pca" or "df")
+function pca_df(key){
+    return function(value){
+        var csvLines = [[], []]
+
+        if(typeof value == "string"){ //caso do esqueleto
+            csvLines[0].push(protect(key.toUpperCase()))
+            csvLines[1].push(protect(value))
+        }else
+            csvLines = convertOne(value, key)
+
+        return csvLines
+    }
+}
+
+//parse filhos (children) of a classe
+function filhos(value){
+    var csvLines = []
+
+    value.forEach(classe => {
+        var aux = convertOne(classe, "classe")
+        aux.splice(0, 1)
+        csvLines = csvLines.concat(aux)
+    })
+
+    return csvLines
+}
+
+//protect value to put in CSV
 function protect(string){
     if(string != null){
-        if(typeof string === 'string'){
+        if(typeof string === 'string')
             string = string.replace(/"/g,'""')
-        }
         return '"' + string + '"'
     }else
         return '""'
 }
 
-function join(array){
-    return array.join('#\n')
-}
-
+//build a final string
 function joinLines(csvLines){
     var len = csvLines.length
 
-    for(var i=0; i<len; i++){
+    for(var i=0; i<len; i++)
         csvLines[i] = csvLines[i].join(separator)
-    }
 
     var csv = csvLines.join('\n')
     return csv
 }
 
-function json2csvArray(array, key){
-    var csv = []
-    var titles = []
-    var columns = [[]]
-
-    switch(key){
-        case 'notasAp':
-            titles[0] = "Notas de aplicação"
-        case 'notasEx':
-            if(titles.length == 0) titles[0] = "Notas de exclusão"
-            array.forEach(e => {
-                columns[0].push(e.nota)
-            })
-            break
-        case 'exemplosNotasAp':
-            titles[0] = "Exemplos de NA"
-            array.forEach(e => {
-                columns[0].push(e.exemplo)
-            })
-            break
-        case 'termosInd':
-            titles[0] = "Termos Indice"
-            array.forEach(e => {
-                columns[0].push(e.termo)
-            })
-            break
-        case 'participantes':
-            titles[0] = "Participante no processo"
-            titles[1] = "Tipo de intervenção do participante"
-            columns[1] = []
-            array.forEach(e => {
-                columns[1].push(e.participLabel)
-            })
-        case 'donos':
-            if(titles.length == 0) titles[0] = "Donos do processo"
-            array.forEach(e => {
-                columns[0].push(e.sigla)
-            })
-            break
-        case 'processosRelacionados':
-            titles[0] = "Código do processo relacionado"
-            titles[1] = "Título do processo relacionado"
-            titles[2] = "Tipo de relação entre processos"
-            columns[1] = []
-            columns[2] = []
-            array.forEach(e => {
-                columns[0].push(e.codigo)
-                columns[1].push(e.titulo)
-                columns[2].push(e.idRel)
-            })
-            break
-        case 'legislacao':
-            titles[0] = "Diplomas jurídico-administrativos REF Ids"
-            titles[1] = "Diplomas jurídico-administrativos REF Títulos"
-            columns[1] = []
-            array.forEach(e => {
-                columns[0].push(e.idLeg)
-                columns[1].push(e.tipo + ' ' + e.numero)
-            })
-            break
-        default:
-            break
-    }
-
-    if(csv.length == 0){
-        titles.forEach((t, index) => titles[index] = protect(t))
-        columns.forEach((c, index) => columns[index] = protect(join(c)))
-        csv = [titles, columns]
-    }
-
-    return csv
-}
-
-function addJustificacao(csvLines, json, key, k){
-    var crits = []
-    var procs_legs = []
-
-    csvLines[0].push(protect('Critério ' + key.toUpperCase()))
-    csvLines[0].push(protect('ProcRefs/LegRefs ' + key.toUpperCase()))
-
-    json[key][k].forEach(just => {
-        crits.push(just.tipoId)
-
-        if(just.processos.length > 0){
-            var ps = []
-            just.processos.forEach(p => ps.push(p.procId))
-            procs_legs.push('(' + join(ps) + ')')
-        }else if(just.legislacao.length > 0){
-            var ls = []
-            just.legislacao.forEach(l => ls.push(l.legId))
-            procs_legs.push('(' + join(ls) + ')')
-        }else{
-            procs_legs.push('()')
-        }
-    })
-
-    csvLines[1].push(protect(join(crits)))
-    csvLines[1].push(protect(join(procs_legs)))
-}
-
-function convertClasse(json){
+//Convert one object of type
+function convertOne(json, type){
     var csvLines = [[],[]]
 
-    for(var key in json){
-        if(json[key] instanceof Array){
-            if(key == 'filhos'){
-                json[key].forEach(classe => {
-                    var aux = convertClasse(classe)
-                    aux.splice(0, 1)
-                    csvLines = csvLines.concat(aux)
-                })
+    for(var key in convert_to[type]){
+        var k = key.split('_')[0]
+        var header = convert_to[type][key][0]
+
+        if(k in json){
+            var f = convert_to[type][key][1]
+            var value = f(json[k])
+
+            if(header == null){
+                csvLines = csvLines.concat(value)
+            }else if(header == ""){
+                csvLines[0] = csvLines[0].concat(value[0])
+                csvLines[1] = csvLines[1].concat(value[1])
             }else{
-                var aux = json2csvArray(json[key], key)
-                csvLines[0] = csvLines[0].concat(aux[0])
-                csvLines[1] = csvLines[1].concat(aux[1])
+                csvLines[0].push(protect(header))
+                csvLines[1].push(protect(value))
             }
-        }else{
-            switch(key){
-                case 'pca':
-                    if(typeof json[key] == "string"){ //caso do esqueleto
-                        csvLines[0].push(protect('PCA'))
-                        csvLines[1].push(protect(json[key]))
-                    }else{
-                        csvLines[0].push("Prazo de conservação administrativa")
-                        csvLines[0].push('Nota ao PCA')
-                        csvLines[0].push('Forma de contagem do PCA')
-                        csvLines[0].push('Sub Forma de contagem do PCA')
-
-                        var index = csvLines[1].length
-                        csvLines[1] = csvLines[1].concat(["", "", "", ""])
-
-                        for(var k in json[key]){
-                            if(k == 'valores'){
-                                csvLines[1][index] = protect(json[key][k])
-                            }else if(k == 'notas'){
-                                csvLines[1][index + 1] = protect(json[key][k])
-                            }else if(k == 'formaContagem'){
-                                csvLines[1][index + 2] = protect(json[key][k])
-                            }else if(k == 'subFormaContagem'){
-                                csvLines[1][index + 3] = protect(json[key][k])
-                            }else if(k == 'justificacao'){
-                                addJustificacao(csvLines, json, key, k)
-                            }
-                        }
-                    }
-                    break
-                case 'df':
-                    if(typeof json[key] == "string"){ //caso do esqueleto
-                        csvLines[0].push(protect('DF'))
-                        csvLines[1].push(protect(json[key]))
-                    }else{
-                        csvLines[0].push("Destino final")
-                        csvLines[0].push('Notas ao DF')
-
-                        var index = csvLines[1].length
-                        csvLines[1] = csvLines[1].concat(["", ""])
-
-                        for(var k in json[key]){
-                            if(k == 'valor'){
-                                if(json[key][k] == "NE"){
-                                    csvLines[1][index] = protect("")
-                                }else{
-                                    csvLines[1][index] = protect(json[key][k])
-                                }
-                            }else if(k == 'notas'){
-                                csvLines[1][index + 1] = protect(json[key][k])
-                            }else if(k == 'justificacao'){
-                                addJustificacao(csvLines, json, key, k)
-                            }
-                        }
-                    }
-                    break
-                case 'codigo':
-                    csvLines[0].push(protect('Código'))
-                    csvLines[1].push(protect(json[key]))
-                    break
-                case 'titulo':
-                    csvLines[0].push(protect('Título'))
-                    csvLines[1].push(protect(json[key]))
-                    break
-                case 'descricao':
-                    csvLines[0].push(protect('Descrição'))
-                    csvLines[1].push(protect(json[key]))
-                    break
-                case 'tipoProc':
-                    csvLines[0].push(protect('Tipo de processo'))
-                    csvLines[1].push(protect(json[key]))
-                    break
-                case 'procTrans':
-                    csvLines[0].push(protect('Processo transversal (S/N)'))
-                    csvLines[1].push(protect(json[key]))
-                    break
-                case 'dono': //caso do esqueleto
-                    csvLines[0].push(protect('Dono'))
-                    csvLines[1].push(protect(json[key]))
-                    break
-                case 'participante': //caso do esqueleto
-                    csvLines[0].push(protect('Participante'))
-                    csvLines[1].push(protect(json[key]))
-                    break
-                default:
-                    break
-            }
-        }
-    }
-    
-    return csvLines
-}
-
-function convertObject(json, type){
-    var csvLines = [[],[]]
-    
-    csvLines[0].push(protect("Sigla"))
-    csvLines[1].push(protect(json.sigla))
-
-    csvLines[0].push(protect("Designação"))
-    csvLines[1].push(protect(json.designacao))
-
-    csvLines[0].push(protect("Estado"))
-    csvLines[1].push(protect(json.estado))
-
-    if(type == "entidade" || type == "entidades"){
-        csvLines[0].push(protect("ID SIOE"))
-        csvLines[1].push(protect(json.sioe))
-
-        csvLines[0].push(protect("Internacional"))
-        if(json.internacional == ""){
-            csvLines[1].push(protect("Não"))
-        }else{
-            csvLines[1].push(protect("Sim"))
+        }else if(type == "pca" || type == "df"){
+            csvLines[0].push(protect(header))
+            csvLines[1].push(protect(""))
         }
     }
 
-    if(json.dono){
-        csvLines[0].push(protect("Dono no processo"))
-        csvLines[1].push(protect(join(json.dono.map(p => p.codigo))))
-    }
-
-    if(json.participante){
-        csvLines[0].push(protect("Participante no processo"))
-        csvLines[1].push(protect(join(json.participante.map(p => p.codigo))))
-
-        csvLines[0].push(protect("Tipo de intervenção no processo"))
-        csvLines[1].push(protect(join(json.participante.map(p => p.tipoPar))))
-    }
-
-    if((type == "entidade" || type == "entidades") && json.tipologias){
-        csvLines[0].push(protect("Tipologias da entidade"))
-        csvLines[1].push(protect(join(json.tipologias.map(t => t.sigla))))
-    }
-
     return csvLines
 }
 
-function convertLegislacao(json, type){
-    var csvLines = [[],[]]
-    
-    csvLines[0].push(protect("Tipo"))
-    csvLines[1].push(protect(json.tipo))
-
-    csvLines[0].push(protect("Número"))
-    csvLines[1].push(protect(json.numero))
-
-    csvLines[0].push(protect("Data"))
-    csvLines[1].push(protect(json.data))
-
-    csvLines[0].push(protect("Sumário"))
-    csvLines[1].push(protect(json.sumario))
-
-    csvLines[0].push(protect("Fonte"))
-    csvLines[1].push(protect(json.fonte))
-
-    csvLines[0].push(protect("Link"))
-    csvLines[1].push(protect(json.link))
-
-    csvLines[0].push(protect("Entidades"))
-    if(type == "legislacoes"){
-        csvLines[1].push(protect(join(json.entidades.map(t => t.sigla))))
-    }else if(type == "legislacao"){
-        csvLines[1].push(protect(join(json.entidades)))
-    }
-
-    if(json.regula){
-        csvLines[0].push(protect("Regula processo"))
-        csvLines[1].push(protect(join(json.regula.map(p => p.codigo))))
-    }
-
-    return csvLines
-}
-
-function convertObjects(json, convObj, type){
+//convert list objects of type
+function convertAll(json, type){
     var csvLines = []
     var len = json.length
 
     if(len > 0){
-        csvLines = convObj(json[0], type)
+        csvLines = convertOne(json[0], type)
 
         for(var i = 1; i < len; i++){
-            var aux = convObj(json[i], type)
+            var aux = convertOne(json[i], type)
             aux.splice(0, 1)
             csvLines = csvLines.concat(aux)
         }
@@ -333,34 +234,25 @@ function convertObjects(json, convObj, type){
 }
 
 module.exports.json2csv = (json, type) => {
-    var csv
+    var csvLines
 
-    switch(type){
-        case "classes":
-            csvLines = convertObjects(json, convertClasse)
-            break
-        case "classe":
-            csvLines = convertClasse(json)
-            break
-        case "entidades":
-        case "tipologias":
-            csvLines = convertObjects(json, convertObject, type)
-            break
-        case "entidade":
-        case "tipologia":
-            csvLines = convertObject(json, type)
-            break
-        case "legislacoes":
-            csvLines = convertObjects(json, convertLegislacao, type)
-            break
-        case "legislacao":
-            csvLines = convertLegislacao(json, type)
-            break
-        default:
-            throw("Não é possível exportar para CSV nesta rota...")
-            break
+    //o tipo é válido?
+    if(types.includes(type)){
+        //lista ou apenas um elemento?
+        if(type.slice(-1) == "s"){
+            type = type.slice(0, -1)
+
+            if(type == "legislacoe"){
+                type = "legislacao"
+            }
+
+            csvLines = convertAll(json, type)
+        }else{
+            csvLines = convertOne(json, type)
+        }
+    }else{
+        throw("Não é possível exportar para CSV nesta rota...")
     }
 
-    csv = joinLines(csvLines)
-    return csv
+    return joinLines(csvLines)
 }
