341b1fa164e82c9fa0eff24d146ae901a1f52c6f
004Ryu <eduardar_125@hotmail.com>
2020-03-06 17:20:01 +0000

    Rotas de criar notificaçao e de ver notificaçoes de um user corrigidas (/terminadas?)

--- routes/api/notificacoes.js
+++ routes/api/notificacoes.js
@@ -5,14 +5,14 @@ var express = require('express');
 var router = express.Router();
 
 // Devolve das notificações de um user
-router.get('/', Auth.isLoggedInKey, (req, res) => {
-    Notificacao.getByUser(req.body.id)
+router.get('/', (req, res) => {
+    Notificacao.getByUser(req.params.user)
         .then(dados => res.jsonp(dados))
         .catch(erro => res.status(500).send(`Erro na obtenção das notificações do utilizador ${req.body.id}: ${erro}`))
 })
 
 // Cria uma nova notificação
-router.post('/', Auth.isLoggedInKey, (req, res) => {
+router.post('/', (req, res) => {
     Notificacao.criar(req.body)
         .then(dados => res.jsonp(dados))
         .catch(erro => res.status(500).send(`Erro na criação da notificação: ${erro}`))
