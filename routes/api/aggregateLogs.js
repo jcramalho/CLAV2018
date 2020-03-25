0467b9b87dd8fecbc823a6923972ac0321a0ebdc
jcm300 <zeclmartins@gmail.com>
2020-03-25 15:01:27 +0000

    Validação de parâmetros da dos logsAgregados; Melhorias na doc para os logsAgregados

--- routes/api/aggregateLogs.js
+++ routes/api/aggregateLogs.js
@@ -1,16 +1,22 @@
 var Auth = require('../../controllers/auth.js');
 var AggregateLogs = require('../../controllers/api/aggregateLogs.js');
+const { query, validationResult } = require('express-validator');
 
 var express = require('express');
 var router = express.Router();
 
-router.get('/', Auth.isLoggedInUser, Auth.checkLevel(6), async (req, res) => {
+router.get('/',
+    Auth.isLoggedInUser, Auth.checkLevel(6), //Auth validation
+    query('tipo').isIn(['User', 'Chave']), query('id').exists({checkNull: true, checkFalsy: true}), //parameters validation
+(req, res) => {
+    
+    try {
+        validationResult(req).throw() //throw error if parameters isn't valid
 
-    if (req.query.tipo && req.query.id) {
         AggregateLogs.getAggregateLog(req.query.id, req.query.tipo)
             .then(data => res.jsonp(data))
             .catch(error => res.status(500).send(`Erro ao obter os logs agregados de ${req.query.tipo} com o id ${req.query.id}: ${error}`))
-    } else {
+    }catch(err){
         AggregateLogs.getAllAggregateLogs()
             .then(data => res.jsonp(data))
             .catch(error => res.status(500).send(`Erro ao obter os logs agregados: ${error}`))
