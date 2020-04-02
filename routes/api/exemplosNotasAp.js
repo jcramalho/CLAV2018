a12728c5906e0815e10772ae50f72f947000ac01
jcm300 <zeclmartins@gmail.com>
2020-04-02 18:02:08 +0100

    Validação de parâmetros da rota /exemplosNotasAp e algumas melhorias; Melhorias na doc para /exemplosNotasAp

--- routes/api/exemplosNotasAp.js
+++ routes/api/exemplosNotasAp.js
@@ -1,12 +1,21 @@
 var Auth = require('../../controllers/auth.js');
 var ExemplosNotasAp = require('../../controllers/api/exemplosNotasAp.js');
 var State = require('../../controllers/state.js')
+const { validationResult } = require('express-validator');
+const { existe } = require('../validation')
 
 var express = require('express');
 var router = express.Router();
 
 // Verifica se umo determinado exemplo de notaAplicação já existe
-router.get('/exemploNotaAp', Auth.isLoggedInKey, (req, res) => {
+router.get('/exemploNotaAp', Auth.isLoggedInKey, [
+    existe('query', 'valor')
+], (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(422).jsonp(errors.array())
+    }
+
     State.verificaExemploNA(req.query.valor)
         .then(dados => res.jsonp(dados))
         .catch(erro => res.status(500).send(`Erro na verificação de um exemplo de nota de aplicação: ${erro}`))
