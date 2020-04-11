d1e6d262c0cd9ed6aad25b9ca3f70d5411d82430
004Ryu <eduardar_125@hotmail.com>
2020-04-11 16:31:52 +0100

    Rota para o numero de tipologias adicionada

--- routes/api/stats.js
+++ routes/api/stats.js
@@ -5,7 +5,7 @@ var ApiStats = require('../../controllers/api/stats');
 var Classes = require('../../controllers/api/classes.js');
 var Entidades = require("../../controllers/api/entidades.js");
 var Leg = require("../../controllers/api/leg.js");
-// var Tipologias = require("../../controllers/api/tipologias.js");
+var Tipologias = require("../../controllers/api/tipologias.js");
 
 router.get('/', Auth.isLoggedInUser, Auth.checkLevel(6), (req, res) => {
     ApiStats.getStats(function(err, result){
@@ -81,9 +81,38 @@ router.get('/legativos', Auth.isLoggedInKey, (req, res) => {
     Leg.getAtivas()
         .then(dados => res.jsonp(dados))
         .catch(erro => res.status(500).send(`Erro na consulta do numero de Documentos Legislativos ativos : ${erro}`))
-  })
+})
 
 //=====================Tipologias=====================//
 
+// Devolve o numero de Tipologias
+router.get('/tipologias', Auth.isLoggedInKey, async (req, res) => {
+    var filtro = [
+        `?estado = "${req.query.estado ? req.query.estado : "Ativa"}"`,
+        req.query.designacao
+          ? `?designacao = "${req.query.designacao}"`
+          : undefined,
+        req.query.tips
+          ? `?uri IN (${req.query.tips
+              .split(",")
+              .map(t => `clav:${t}`)
+              .join(",")})`
+          : undefined
+      ]
+        .filter(v => v !== undefined)
+        .join(" && ");
+    
+    try {
+    var lista = await Tipologias.listar(filtro);
+    var result = {
+        indicador: "Numero de tipologias",
+        valor: Object.keys(lista).length
+    }
+    res.jsonp(result);
+    }catch(erro) {
+        res.status(500).send(`Erro na contagem das tipologias: ${erro}`);
+    }
+    
+})
 
 module.exports = router;
