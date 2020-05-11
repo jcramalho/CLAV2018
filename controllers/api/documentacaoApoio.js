9880a91e40f420f55952c61b174e279519ace699
citoplasme <jony.pi10@gmail.com>
2020-05-11 21:14:26 +0100

    Documentação de Apoio

---
+++ controllers/api/documentacaoApoio.js
@@ -0,0 +1,226 @@
+const DocApoio = require('../../models/documentacaoApoio');
+const DocumentacaoApoio = module.exports;
+var mongoose = require('mongoose');
+
+// -------------------------------------- Consultas --------------------------------
+
+DocumentacaoApoio.listar = (filtro) => {
+    return DocApoio.find(filtro);
+};
+
+DocumentacaoApoio.listar_classes = () => {
+    return DocApoio.distinct("classe");
+};
+
+DocumentacaoApoio.consultar = (id) => {
+    return DocApoio.findOne({ _id: id });
+};
+
+DocumentacaoApoio.consultar_entradas = (id) => {
+    return DocApoio.findOne({ _id: id}, {entradas : 1});
+};
+
+DocumentacaoApoio.consultar_entrada = (id, entrada) => {
+    //return DocApoio.findOne({ _id: id, "entradas._id" : entrada });
+    let idM = mongoose.Types.ObjectId(id);
+    let idE = mongoose.Types.ObjectId(entrada);
+    return DocApoio.aggregate([ 
+        { $match : 
+            {"_id" : idM}
+        }, 
+        { $unwind : "$entradas"},
+        { $match : 
+            {"entradas._id" : idE}
+        },
+        { $project : 
+            {
+                _id : "$entradas._id",
+                descricao : "$entradas.descricao",
+                elementos : "$entradas.elementos"
+            }
+        }
+    ]);
+};
+
+DocumentacaoApoio.consultar_elementos = (id, entrada) => {
+    let idM = mongoose.Types.ObjectId(id);
+    let idE = mongoose.Types.ObjectId(entrada);
+    return DocApoio.aggregate([ 
+        { $match : 
+            {"_id" : idM}
+        }, 
+        { $unwind : "$entradas"},
+        { $match : 
+            {"entradas._id" : idE}
+        },
+        { $project : 
+            {
+                _id : "$entradas._id",
+                elementos : "$entradas.elementos"
+            }
+        }
+    ]);
+};
+
+DocumentacaoApoio.consultar_elemento = (id, entrada, elemento) => {
+    let idM = mongoose.Types.ObjectId(id);
+    let idE = mongoose.Types.ObjectId(entrada);
+    let idEle = mongoose.Types.ObjectId(elemento);
+    return DocApoio.aggregate([ 
+        { $match : 
+            {"_id" : idM}
+        }, 
+        { $unwind : "$entradas"},
+        { $unwind : "$entradas.elementos"},
+        { $match : 
+            {"entradas._id" : idE, "entradas.elementos._id" : idEle}
+        },
+        { $project : 
+            {
+                _id : "$entradas.elementos._id",
+                visivel : "$entradas.elementos.visivel",
+                texto : "$entradas.elementos.texto",
+                ficheiro : "$entradas.elementos.ficheiro"
+            }
+        }
+    ]);
+};
+
+DocumentacaoApoio.consultar_ficheiro = (id, entrada, elemento) => {
+    let idM = mongoose.Types.ObjectId(id);
+    let idE = mongoose.Types.ObjectId(entrada);
+    let idEle = mongoose.Types.ObjectId(elemento);
+    return DocApoio.aggregate([ 
+        { $match : 
+            {"_id" : idM}
+        }, 
+        { $unwind : "$entradas"},
+        { $unwind : "$entradas.elementos"},
+        { $match : 
+            {"entradas._id" : idE, "entradas.elementos._id" : idEle}
+        },
+        { $project : 
+            {
+                nome: "$entradas.elementos.ficheiro.nome",
+                path: "$entradas.elementos.ficheiro.path",
+                data: "$entradas.elementos.ficheiro.data",
+                mimetype: "$entradas.elementos.ficheiro.mimetype",
+                size: "$entradas.elementos.ficheiro.size",
+            }
+        }
+    ]);
+};
+
+// ---------------------------------- Adições ------------------------------------
+
+DocumentacaoApoio.criar_classe = c => {
+    let n = {
+        _id : mongoose.Types.ObjectId(),
+        classe : c,
+        entradas : []
+    }
+        
+    var newDocApoio = new DocApoio(n);
+    return newDocApoio.save(); 
+}
+
+DocumentacaoApoio.criar_elemento = (classe, entrada, elemento) => {
+    elemento._id = mongoose.Types.ObjectId();
+    
+    return DocApoio.findOneAndUpdate(
+        { _id : classe, "entradas._id" : entrada}, 
+        { $push : { "entradas.$[elem].elementos" : elemento}},
+        { arrayFilters : [{ "elem._id": entrada }]})
+        .exec()
+    
+}
+
+DocumentacaoApoio.criar_entrada = (classe, entrada) => {
+    let ent = {
+        _id: mongoose.Types.ObjectId(),
+        descricao: entrada,
+        elementos: []
+    };
+    
+    return DocApoio.findOneAndUpdate(
+        { _id : classe}, 
+        { $push : { entradas : ent}})
+        .exec()
+    
+}
+
+// ---------------------------------- Atualizações -------------------------------------
+
+DocumentacaoApoio.editar_classe = (id, classe) => {
+    return DocApoio
+        .update({_id : id}, classe, {overwrite: true })
+        .exec();
+}
+
+DocumentacaoApoio.editar_entrada = (id, entrada, desc) => {
+    return DocApoio.findOneAndUpdate(
+        { _id : id, "entradas._id" : entrada}, 
+        { $set : { "entradas.$[elem].descricao" : desc}},
+        { arrayFilters : [{ "elem._id": entrada }]})
+        .exec()    
+}
+
+DocumentacaoApoio.editar_elemento = (id, entrada, elemento, objeto) => {
+    return DocApoio.findOneAndUpdate(
+        { _id : id, "entradas._id" : entrada}, 
+        { $set : { "entradas.$[elem].elementos.$[i]" : objeto}},
+        { arrayFilters : [{ "elem._id": entrada }, { "i._id": elemento }]})
+        .exec()    
+}
+
+// ---------------------------------- Eliminações --------------------------------------
+
+DocumentacaoApoio.eliminar_elemento = function(classe, entrada, elemento, callback) {
+    return DocApoio.findOneAndUpdate(
+        { _id : classe, "entradas._id" : entrada}, 
+        { $pull : { "entradas.$[elem].elementos" : { _id : elemento }}},
+        { arrayFilters : [{ "elem._id": entrada }]}, function(err, documento){
+            if(err){
+                callback(err, null);
+            } else {
+                callback(null, documento);
+            }
+        })
+        .exec()
+}
+
+DocumentacaoApoio.eliminar_entrada = function(classe, entrada, callback) {
+    return DocApoio.findOneAndUpdate(
+        { _id : classe}, 
+        { $pull : { entradas : { _id : entrada }}}, function(err, documento){
+            if(err){
+                callback(err, null);
+            } else {
+                callback(null, documento);
+            }
+        })
+        .exec()
+}
+
+DocumentacaoApoio.eliminar = function(id, callback){
+    DocApoio.findOneAndRemove({_id: id}, function(err, documento){
+        if (err) {	
+            callback(err, null);
+        } else if(!documento){
+            try{
+                id = mongoose.Types.ObjectId(id)
+                DocApoio.findOneAndRemove({_id: id}, function(err2, documento2){
+                    if(err2){
+                        callback(err2, null);
+                    }else{
+                        callback(null, documento2);
+                    }
+                })
+            }catch(e){
+                callback("Documentação não existe", null)
+            }
+        } else {
+		    callback(null, documento);
+        }
+    });
+}
