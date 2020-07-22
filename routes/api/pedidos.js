2c8e43739080401285979fa118b2e8e719729471
Ricardo Canela <ricardocanela.ns@gmail.com>
2020-07-22 16:08:29 +0100

    minimal changes

--- routes/api/pedidos.js
+++ routes/api/pedidos.js
@@ -1,5 +1,6 @@
 var Auth = require('../../controllers/auth.js');
 var Pedidos = require('../../controllers/api/pedidos');
+var Notificacao = require('../../controllers/api/notificacoes')
 var express = require('express');
 var router = express.Router();
 
@@ -71,7 +72,6 @@ router.post('/', Auth.isLoggedInUser, Auth.checkLevel([1, 3, 3.5, 4, 5, 6, 7]), 
 
     Pedidos.criar(req.body)
         .then(dados => {
-            
             var notificacao = {
                 entidade : req.body.entidade,
                 pedido: req.body.novoObjeto.codigo,
@@ -79,6 +79,7 @@ router.post('/', Auth.isLoggedInUser, Auth.checkLevel([1, 3, 3.5, 4, 5, 6, 7]), 
                 tipo: req.body.tipoObjeto,
                 novoEstado: "Submetido"
             }
+
             Notificacao.criar(notificacao);
 
             res.jsonp(dados);
