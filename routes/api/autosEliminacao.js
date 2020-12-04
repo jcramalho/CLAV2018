f2440d18103d05804e58126bd2ce8660cf49672c
Manuel <manueljoao1996@hotmail.com>
2020-12-04 14:21:45 +0000

    TS: Implementação da importação de TS com diferentes fontes de legitimação

--- routes/api/autosEliminacao.js
+++ routes/api/autosEliminacao.js
@@ -1,102 +1,146 @@
-var Auth = require('../../controllers/auth.js');
-var AutosEliminacao = require('../../controllers/api/autosEliminacao.js');
-var User = require('../../controllers/api/users.js')
-var excel2Json = require('../../controllers/conversor/xslx2json')
-var xml2Json = require('../../controllers/conversor/aeXml2Json')
+var Auth = require("../../controllers/auth.js");
+var AutosEliminacao = require("../../controllers/api/autosEliminacao.js");
+var User = require("../../controllers/api/users.js");
+var excel2Json = require("../../controllers/conversor/xslx2json");
+var xml2Json = require("../../controllers/conversor/aeXml2Json");
 var xml = require("libxmljs");
-var xml2js = require('xml2js')
-var fs = require("fs")
+var xml2js = require("xml2js");
+var fs = require("fs");
 
-const { validationResult } = require('express-validator');
-const { existe, estaEm, verificaAEId, vcTipoAE } = require('../validation')
+const { validationResult } = require("express-validator");
+const {
+  existe,
+  estaEm,
+  verificaAEId,
+  vcTipoAE,
+  vcFonte,
+} = require("../validation");
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var formidable = require("formidable")
+var formidable = require("formidable");
 
-router.get('/', Auth.isLoggedInKey, (req, res) => {
-    AutosEliminacao.listar()
-        .then(dados => res.jsonp(dados))
-        .catch(erro => res.status(404).jsonp("Erro na listagem dos AE: " + erro))
-})
+router.get("/", Auth.isLoggedInKey, (req, res) => {
+  AutosEliminacao.listar()
+    .then((dados) => res.jsonp(dados))
+    .catch((erro) => res.status(404).jsonp("Erro na listagem dos AE: " + erro));
+});
 
-router.get('/:id', Auth.isLoggedInKey, [
-    verificaAEId('param', 'id')
-], function (req, res) {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(422).jsonp(errors.array())
-    }
-    
-    AutosEliminacao.consultar(req.params.id, req.user ? req.user.entidade : null)
-        .then(dados => res.jsonp(dados))
-        .catch(erro => res.status(404).jsonp("Erro na consulta do AE "+req.params.id+": " + erro))
-})
+router.get("/:id", Auth.isLoggedInKey, [verificaAEId("param", "id")], function (
+  req,
+  res
+) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).jsonp(errors.array());
+  }
+
+  AutosEliminacao.consultar(req.params.id, req.user ? req.user.entidade : null)
+    .then((dados) => res.jsonp(dados))
+    .catch((erro) =>
+      res
+        .status(404)
+        .jsonp("Erro na consulta do AE " + req.params.id + ": " + erro)
+    );
+});
 
 //Criar um AE && Importar AE
-router.post('/', Auth.isLoggedInUser, Auth.checkLevel([5, 6, 7]), [
-    existe('body', 'auto')
-], (req, res) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(422).jsonp(errors.array())
+router.post(
+  "/",
+  Auth.isLoggedInUser,
+  Auth.checkLevel([5, 6, 7]),
+  [existe("body", "auto")],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).jsonp(errors.array());
     }
 
     AutosEliminacao.adicionar(req.body.auto)
-        .then(dados => res.jsonp(dados))
-        .catch(err => res.status(500).send(`Erro na criação de auto de eliminação: ${err}`))
-        
-})
+      .then((dados) => res.jsonp(dados))
+      .catch((err) =>
+        res.status(500).send(`Erro na criação de auto de eliminação: ${err}`)
+      );
+  }
+);
 
 //Importar um AE (Inserir ficheiro diretamente pelo Servidor)
-router.post('/importar', Auth.isLoggedInUser, Auth.checkLevel([1, 3, 3.5, 4, 5, 6, 7]), [
-    estaEm('query', 'tipo', vcFonte)
-], (req, res) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(422).jsonp(errors.array())
+router.post(
+  "/importar",
+  Auth.isLoggedInUser,
+  Auth.checkLevel([1, 3, 3.5, 4, 5, 6, 7]),
+  [estaEm("query", "tipo", vcFonte)],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).jsonp(errors.array());
     }
 
-    var form = new formidable.IncomingForm()
+    var form = new formidable.IncomingForm();
     form.parse(req, async (error, fields, formData) => {
-        if(error) res.status(500).send(`Erro ao importar Auto de Eliminação: ${error}`)
-        else if(!formData.file || !formData.file.path) res.status(500).send(`Erro ao importar Auto de Eliminação: É necessário o campo file`)
-        else if(formData.file.type=="text/xml") {
-            var tipoAE = req.query.tipo
-            if((tipoAE == 'TS/LC')||(tipoAE == 'PGD/LC'))
-                var schemaPath = __dirname+"/../../public/schema/autoEliminacao.xsd"
-            else
-                var schemaPath = __dirname+"/../../public/schema/autoEliminacaoOld.xsd"
+      if (error)
+        res.status(500).send(`Erro ao importar Auto de Eliminação: ${error}`);
+      else if (!formData.file || !formData.file.path)
+        res
+          .status(500)
+          .send(
+            `Erro ao importar Auto de Eliminação: É necessário o campo file`
+          );
+      else if (formData.file.type == "text/xml") {
+        var tipoAE = req.query.tipo;
+        if (tipoAE == "TS/LC" || tipoAE == "PGD/LC")
+          var schemaPath =
+            __dirname + "/../../public/schema/autoEliminacao.xsd";
+        else
+          var schemaPath =
+            __dirname + "/../../public/schema/autoEliminacaoOld.xsd";
 
-            var schema = await fs.readFileSync(schemaPath)
-            var xsl = xml.parseXml(schema)
-            var doc = await fs.readFileSync(formData.file.path)
-            var xmlDoc = xml.parseXml(doc)
+        var schema = await fs.readFileSync(schemaPath);
+        var xsl = xml.parseXml(schema);
+        var doc = await fs.readFileSync(formData.file.path);
+        var xmlDoc = xml.parseXml(doc);
 
-            if(xmlDoc.validate(xsl)) {
-                const parser = new xml2js.Parser()
-                parser.parseString(doc, (error, result) => {
-                    if(error) res.status(500).send("Erro na leitura do ficheiro .xml")
-                    else {
-                        User.getUserById(req.user.id, function (err, user) {
-                            if(err) res.status(500).json(`Erro na consulta de utilizador para importação do AE: ${err}`)
-                            else {
-                                xml2Json(result.auto, req.query.tipo)
-                                    .then(data =>{
-                                        AutosEliminacao.importar(data.auto, req.query.tipo, user)
-                                            .then(dados => {
-                                                res.jsonp(req.query.tipo+" adicionado aos pedidos com sucesso com codigo: "+dados.codigo)
-                                            })
-                                            .catch(erro => res.status(500).json(`Erro na adição do AE: ${erro}`))
-                                    })
-                                    .catch(err => res.status(500).send(err))
-                            }
-                        });
-                    }
-                })
-            } else res.status(500).send(xmlDoc.validationErrors)
-        } else res.status(500).send(`Erro ao importar Auto de Eliminação, tipo de ficheiro: ${formData.file.type}`)
-    })
-})
- 
+        if (xmlDoc.validate(xsl)) {
+          const parser = new xml2js.Parser();
+          parser.parseString(doc, (error, result) => {
+            if (error) res.status(500).send("Erro na leitura do ficheiro .xml");
+            else {
+              User.getUserById(req.user.id, function (err, user) {
+                if (err)
+                  res
+                    .status(500)
+                    .json(
+                      `Erro na consulta de utilizador para importação do AE: ${err}`
+                    );
+                else {
+                  xml2Json(result.auto, req.query.tipo)
+                    .then((data) => {
+                      AutosEliminacao.importar(data.auto, req.query.tipo, user)
+                        .then((dados) => {
+                          res.jsonp(
+                            req.query.tipo +
+                              " adicionado aos pedidos com sucesso com codigo: " +
+                              dados.codigo
+                          );
+                        })
+                        .catch((erro) =>
+                          res.status(500).json(`Erro na adição do AE: ${erro}`)
+                        );
+                    })
+                    .catch((err) => res.status(500).send(err));
+                }
+              });
+            }
+          });
+        } else res.status(500).send(xmlDoc.validationErrors);
+      } else
+        res
+          .status(500)
+          .send(
+            `Erro ao importar Auto de Eliminação, tipo de ficheiro: ${formData.file.type}`
+          );
+    });
+  }
+);
+
 module.exports = router;
