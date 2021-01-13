520fcaa4e2bdf4fab3c94741337da45081c37940
José Carlos Ramalho <jcramalho@users.noreply.github.com>
2021-01-13 21:25:41 +0000

    AE: Conversão XML -> JSON.

--- controllers/conversor/aeXml2Json.js
+++ controllers/conversor/aeXml2Json.js
@@ -14,7 +14,7 @@ var aeConverter = function(obj,tipo) {
     console.log('FL: ' + fonteLegTipo + ', ' + fonteLegDiploma)
     var fundos = obj.fundos.map(f => { return f.fundo[0]})
     console.dir('fundos: ' + JSON.stringify(fundos))
-    var classes = obj.classes
+    var classes = obj.classes.map(c => c.classe[0])
     console.dir('classes: ' + JSON.stringify(classes))
 
     /*if(tipo=="TS/LC") {
