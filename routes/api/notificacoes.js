e5382d1f4b36eb74d7f426fd656cb3f5fca322d3
José Carlos Ramalho <jcramalho@users.noreply.github.com>
2021-09-02 23:01:04 +0100

    Retiramos a proteção nas notificações.

--- routes/api/notificacoes.js
+++ routes/api/notificacoes.js
@@ -1,14 +1,13 @@
 
 var Notificacoes = require('../../controllers/api/notificacoes.js')
-var Auth = require('../../controllers/auth.js');
 
 var express = require('express');
 var router = express.Router();
 
 // Devolve as notificações do user atual
-router.get('/', Auth.isLoggedInKey, Notificacoes.get);
+router.get('/', Notificacoes.get);
 
 // Apaga uma notificação pelo id
-router.delete('/:idNotificacao', Auth.isLoggedInKey, Notificacoes.delete);
+router.delete('/:idNotificacao', Notificacoes.delete);
 
 module.exports = router;
