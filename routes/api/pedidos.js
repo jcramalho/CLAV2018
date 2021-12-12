19fd9c55830a4245823468ef0a45dc516fb45305
José Carlos Ramalho <jcramalho@users.noreply.github.com>
2021-12-12 13:00:39 +0000

    Nova rota para consulta do estado do pedido.

--- routes/api/pedidos.js
+++ routes/api/pedidos.js
@@ -86,9 +86,10 @@ router.get('/:codigo/estado', Auth.isLoggedInUser, Auth.checkLevel([1, 2, 3, 3.5
     Pedidos.consultar(req.params.codigo)
         .then(dados => {
             if(dados){
+                    d = JSON.stringify(dados.data)
                     res.jsonp({
                         pedido: req.params.codigo,
-                        data: dados.data,
+                        data: d.substr(0,10),
                         mensagem: "O seu pedido foi rececionado em ???" + 
                             " e encontra-se em análise."
                     })
