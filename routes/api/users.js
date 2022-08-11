33664d3facd6fea0c65567bd16f62515fbe31c53
José Carlos Ramalho <jcramalho@users.noreply.github.com>
2022-08-11 09:29:11 +0100

    Acrescentei o passport que tinha desaparecido com o merge.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var Logging = require('../../controllers/logging');
 var router = express.Router();
+var passport = require('passport');
 var User = require('../../models/user');
 var Users = require('../../controllers/api/users');
 var AuthCalls = require('../../controllers/api/auth');
