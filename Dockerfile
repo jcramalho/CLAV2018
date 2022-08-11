141324c362d7b48314768d4b4d066a92de95a24c
José Carlos Ramalho <jcramalho@users.noreply.github.com>
2022-08-11 19:21:48 +0100

    Desfiz alguns commits.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14.5
+FROM node:16.16.0
 COPY ./ /server
 WORKDIR /server
 RUN npm install
--- routes/api/users.js
+++ routes/api/users.js
@@ -177,13 +177,12 @@ router.post("/login", [
         return res.status(422).jsonp(errors.array())
     }
 
-    Users.getUserByEmail(req.body.username, function (err, user) {
-        if (err) 
-            //return res.status(500).send(`Erro: ${err}`);
-            return res.status(500).send("Não foi possível proceder a autenticação!");
+    passport.authenticate('login', (err, user, info) => {
+        if (err)
+            //res.status(500).send(err)
+            res.status(500).send("Não foi possível proceder a autenticação!")
         if (!user)
-            //Não existe nenhum utilizador registado com esse email
-            return res.status(401).send('Credenciais inválidas');
+            res.status(401).send('Credenciais inválidas')
         else{
             req.login(user, () => {
                 var token = Auth.generateTokenUser(user);
@@ -195,7 +194,7 @@ router.post("/login", [
                 })
             })
         }
-    });
+    })(req, res, next);
 });
 
 router.post('/recuperar', [
@@ -219,7 +218,7 @@ router.post('/recuperar', [
             //Por forma a não divulgar que emails estão já usados, será devolvido que foi enviado um email com sucesso
             res.send('Email enviado com sucesso!');
         else{
-            Users.getUserById(user._id, function(err, user){
+            Users.getUserById(user._id, async function(err, user){
                 if(err)
                     //return res.status(500).send(`Erro: ${err}`);
                     return res.status(500).send("Não foi possível recuperar a conta!");
