6264deaeafaf27aaaa3d5de7893a82f3f720caf0
José Carlos Ramalho <jcramalho@users.noreply.github.com>
2022-10-14 11:26:00 +0100

    Reformulei o reload.js

--- routes/api/reload.js
+++ routes/api/reload.js
@@ -3,13 +3,14 @@ var State = require('../../controllers/state.js');
 var express = require('express');
 var router = express.Router();
 
-router.get('/cache', Auth.isLoggedInUser, Auth.checkLevel(7), async (req, res) => {
-    try{
-        res.send("A realizar o reload da cache do sistema! Pode demorar alguns minutos. Veja o output log do sistema para ver se foi efetuado com sucesso...")
-        await State.reload()
-    } catch(erro) {
-        res.status(500).send("Erro ao realizar o reload da cache sistema: " + erro)
-    }
-})
+router.get('/cache', Auth.isLoggedInUser, Auth.checkLevel(7), (req, res) => {
+    State.reload()
+        .then(() => {
+            res.send("Cache: RELOADED")
+        })
+        .catch(erro => {
+            res.status(500).send("Erro ao realizar o reload da cache sistema: " + erro)
+        })
+    })
 
 module.exports = router;
